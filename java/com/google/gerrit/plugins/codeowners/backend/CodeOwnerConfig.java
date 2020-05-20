d1dcf599f89fa146fa58b409149c014b921d83dd
Edwin Kempin <ekempin@google.com>
2020-05-20 10:18:56 +0200

    Define an abstraction for code owner configurations
    Code owners are defined for folders in branches. Which syntax is used to
    define the code owners and where the code owner configuration is stored
    depends on the code owner backend.
    
    With the CodeOwnerConfig class this change defines an abstraction for
    code owner configurations that is independent of the syntax and the
    storage location.
    
    Since code owners are defined for folders in branches, the key of a
    CodeOwnerConfig is a tuple of project + branch (BranchNameKey) and
    folder.
    
    For now we only support the simplest code owner configuration possible,
    which is a set of code owners. Code owners in CodeOwnerConfigs are
    represented as CodeOwnerReferences.
    
    A CodeOwnerReference defines a code owner in a code owner
    configuration. For now we only support to reference code owners by
    email, but we may support further identifiers later, which is why we
    have CodeOwnerReference as an abstraction instead of using Strings for
    representing code owner emails in CodeOwnerConfig.
    
    This change adds a test that covers the custom functionality of
    CodeOwnerConfig (the AutoValue methods do not need to be tested). Since
    this is the first test of the plugin, add the command to run the plugin
    tests to the documentation.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I7d2337fe2de9762f05ffbdab9165b1770eee4690

---
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerConfig.java
@@ -0,0 +1,136 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import static java.util.Objects.requireNonNull;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.entities.BranchNameKey;
+import java.nio.file.Path;
+
+/** Code owner configuration for a folder in a branch. */
+@AutoValue
+public abstract class CodeOwnerConfig {
+  /** Gets the key of this code owner config. */
+  public abstract Key key();
+
+  /** Gets the code owners of this code owner config. */
+  public abstract ImmutableSet<CodeOwnerReference> codeOwners();
+
+  /**
+   * Creates a builder for a code owner config.
+   *
+   * @param key the key of the code owner config
+   * @return builder for a code owner config
+   */
+  public static Builder builder(Key key) {
+    return new AutoValue_CodeOwnerConfig.Builder().setKey(key);
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    /**
+     * Sets the key of this code owner config.
+     *
+     * @param key the key of this code owner config
+     * @return the Builder instance for chaining calls
+     */
+    public abstract Builder setKey(Key key);
+
+    /**
+     * Sets the code owners of this code owner config.
+     *
+     * @param codeOwners the code owners of this code owner config
+     * @return the Builder instance for chaining calls
+     */
+    public abstract Builder setCodeOwners(ImmutableSet<CodeOwnerReference> codeOwners);
+
+    /** Gets a builder to add code owner references. */
+    abstract ImmutableSet.Builder<CodeOwnerReference> codeOwnersBuilder();
+
+    /**
+     * Adds a code owner.
+     *
+     * @param codeOwnerReference reference to the code owner
+     * @return the Builder instance for chaining calls
+     */
+    public Builder addCodeOwner(CodeOwnerReference codeOwnerReference) {
+      codeOwnersBuilder().add(requireNonNull(codeOwnerReference, "codeOwnerReference"));
+      return this;
+    }
+
+    /**
+     * Builds the {@link CodeOwnerConfig} instance.
+     *
+     * @return the {@link CodeOwnerConfig} instance
+     */
+    public abstract CodeOwnerConfig build();
+  }
+
+  /**
+   * Key of a {@link CodeOwnerConfig}.
+   *
+   * <p>The folder in a branch for which the code owner config defines code owners.
+   */
+  @AutoValue
+  public abstract static class Key {
+    /** Gets the branch to which the code owner config belongs. */
+    public abstract BranchNameKey branch();
+
+    /** Gets the path of the folder to which the code owner config belongs. */
+    public abstract Path folderPath();
+
+    /**
+     * Creates a code owner config key.
+     *
+     * @param branch the branch to which the code owner config belongs
+     * @param folderPath the path of the folder to which the code owner config belongs
+     * @return the code owner config key
+     */
+    public static Key create(BranchNameKey branch, Path folderPath) {
+      return new AutoValue_CodeOwnerConfig_Key.Builder()
+          .setBranch(branch)
+          .setFolderPath(folderPath)
+          .build();
+    }
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      /**
+       * Sets the branch for this owner config key.
+       *
+       * @param branch the branch for this owner config key
+       * @return the Builder instance for chaining calls
+       */
+      public abstract Builder setBranch(BranchNameKey branch);
+
+      /**
+       * Sets the folder path for this owner config key.
+       *
+       * @param folderPath the folder path for this owner config key
+       * @return the Builder instance for chaining calls
+       */
+      public abstract Builder setFolderPath(Path folderPath);
+
+      /**
+       * Builds the {@link Key} instance.
+       *
+       * @return the {@link Key} instance
+       */
+      abstract Key build();
+    }
+  }
+}
---
+++ javatests/com/google/gerrit/plugins/codeowners/backend/CodeOwnerConfigTest.java
@@ -0,0 +1,60 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+
+import com.google.gerrit.entities.BranchNameKey;
+import com.google.gerrit.entities.Project;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+/** Tests for {@link CodeOwnerConfig}. */
+public class CodeOwnerConfigTest {
+  @Test
+  public void addCodeOwners() throws Exception {
+    CodeOwnerReference codeOwner1 = CodeOwnerReference.create("jdoe@example.com");
+    CodeOwnerReference codeOwner2 = CodeOwnerReference.create("jroe@example.com");
+    CodeOwnerConfig.Builder codeOwnerConfigBuilder = createCodeOwnerBuilder();
+    codeOwnerConfigBuilder.addCodeOwner(codeOwner1);
+    codeOwnerConfigBuilder.addCodeOwner(codeOwner2);
+    CodeOwnerConfig codeOwnerConfig = codeOwnerConfigBuilder.build();
+    assertThat(codeOwnerConfig.codeOwners()).containsExactly(codeOwner1, codeOwner2);
+  }
+
+  @Test
+  public void addDuplicateCodeOwners() throws Exception {
+    CodeOwnerReference codeOwner = CodeOwnerReference.create("jdoe@example.com");
+    CodeOwnerConfig.Builder codeOwnerConfigBuilder = createCodeOwnerBuilder();
+    codeOwnerConfigBuilder.addCodeOwner(codeOwner);
+    codeOwnerConfigBuilder.addCodeOwner(codeOwner);
+    CodeOwnerConfig codeOwnerConfig = codeOwnerConfigBuilder.build();
+    assertThat(codeOwnerConfig.codeOwners()).containsExactly(codeOwner);
+  }
+
+  @Test
+  public void cannotAddNullAsCodeOwner() throws Exception {
+    NullPointerException npe =
+        assertThrows(NullPointerException.class, () -> createCodeOwnerBuilder().addCodeOwner(null));
+    assertThat(npe).hasMessageThat().isEqualTo("codeOwnerReference");
+  }
+
+  private static CodeOwnerConfig.Builder createCodeOwnerBuilder() {
+    return CodeOwnerConfig.builder(
+        CodeOwnerConfig.Key.create(
+            BranchNameKey.create(Project.nameKey("project"), "master"), Paths.get("/")));
+  }
+}
