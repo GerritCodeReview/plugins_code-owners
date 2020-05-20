f9dc2dc32f4e776782a3bcd6432c9c63c999b28c
Edwin Kempin <ekempin@google.com>
2020-05-20 10:18:56 +0200

    Define an abstraction for code owner configurations
    Code owners are defined for folders in branches. Which syntax is used to
    define the code owners and where the code owner configuration is stored
    depends on the code owner backend.
    
    With the CodeOwnerConfig class this change defines an abstraction for
    code owner configurations that is independent of the syntax and the
    storage location.
    
    Since code owners are defined for folders in branches, the key of a
    CodeOwnerConfig is a tuple of project + branch (BranchNameKey) and
    folder.
    
    For now we only support the simplest code owner configuration possible,
    which is a set of code owners. Code owners in CodeOwnerConfigs are
    represented as CodeOwnerReferences.
    
    A CodeOwnerReference defines a code owner in a code owner
    configuration. For now we only support to reference code owners by
    email, but we may support further identifiers later, which is why we
    have CodeOwnerReference as an abstraction instead of using Strings for
    representing code owner emails in CodeOwnerConfig.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I7d2337fe2de9762f05ffbdab9165b1770eee4690

---
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerReference.java
@@ -0,0 +1,62 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import com.google.auto.value.AutoValue;
+
+/**
+ * A reference that defines a code owner in code owner configurations.
+ *
+ * <p>At the moment we only support emails to specify code owners in code owner configurations, but
+ * in the future we may support other identifiers to reference code owners, which is why we have
+ * this class as an abstraction.
+ */
+@AutoValue
+public abstract class CodeOwnerReference {
+  /**
+   * Returns the email of the code owner.
+   *
+   * @return email of the code owner.
+   */
+  public abstract String email();
+
+  /**
+   * Creates a reference to a code owner by email.
+   *
+   * @param email the email of the code owner
+   * @return a reference to a code owner by email
+   */
+  public static CodeOwnerReference create(String email) {
+    return new AutoValue_CodeOwnerReference.Builder().setEmail(email).build();
+  }
+
+  @AutoValue.Builder
+  abstract static class Builder {
+    /**
+     * Sets the email of the code owner for the code owner reference.
+     *
+     * @param email the email of the code owner
+     * @return the Builder instance for chaining calls
+     */
+    public abstract Builder setEmail(String email);
+
+    /**
+     * Builds the {@link CodeOwnerReference} instance.
+     *
+     * @return the {@link CodeOwnerReference} instance
+     */
+    abstract CodeOwnerReference build();
+  }
+}
