68e01f39a17894afb30cd48aa3c5a2ef5f799956
Edwin Kempin <ekempin@google.com>
2020-05-27 17:21:35 +0200

    [DO NOT SUBMIT] Support configuring code owners backends per project
    If no code owners backend is configured on project level, the code
    owners backend configuration is inherited from the parent projects. If
    none of the parent projects defines a code owners backend, we fall back
    to the default code owners backend that is configured globally in
    gerrit.config.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I555ccaf9d0b140be75ad5f9969135ad69a61d629

--- java/com/google/gerrit/plugins/codeowners/CodeOwnersPluginConfiguration.java
+++ java/com/google/gerrit/plugins/codeowners/CodeOwnersPluginConfiguration.java
@@ -14,31 +14,48 @@
 
 package com.google.gerrit.plugins.codeowners;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.extensions.registration.DynamicMap;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnersBackend;
 import com.google.gerrit.plugins.codeowners.backend.findowners.FindOwnersBackend;
 import com.google.gerrit.server.config.PluginConfigFactory;
+import com.google.gerrit.server.project.NoSuchProjectException;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.util.Optional;
+import org.eclipse.jgit.lib.Config;
 
 /**
  * The configuration of the code-owners plugin.
  *
- * <p>The configuration of the code-owners plugin is stored globally in the {@code gerrit.config}
- * file in the {@code plugin.code-owners} subsection.
+ * <p>The global configuration of the code-owners plugin is stored in the {@code gerrit.config} file
+ * in the {@code plugin.code-owners} subsection.
+ *
+ * <p>In addition there is configuration on project level that is stored in {@code
+ * code-owners.config} files that are stored in the {@code refs/meta/config} branches of the
+ * projects.
+ *
+ * <p>Parameters that are not set for a project are inherited from the parent project.
  */
 @Singleton
 public class CodeOwnersPluginConfiguration {
-  private static final String KEY_BACKEND = "backend";
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  @VisibleForTesting public static final String KEY_BACKEND = "backend";
+  @VisibleForTesting public static final String SECTION_CODE_OWNERS = "codeOwners";
 
   private final String pluginName;
   private final PluginConfigFactory pluginConfigFactory;
   private final DynamicMap<CodeOwnersBackend> codeOwnersBackends;
 
-  /** The configured code owners backend. */
-  private CodeOwnersBackend codeOwnersBackend;
+  /**
+   * The configured default code owners backend that is used if there is no code owners backend
+   * configuration on project level.
+   */
+  private CodeOwnersBackend defaultCodeOwnersBackend;
 
   @Inject
   CodeOwnersPluginConfiguration(
@@ -53,15 +70,36 @@ public class CodeOwnersPluginConfiguration {
   /**
    * Returns the configured {@link CodeOwnersBackend}.
    *
+   * <p>Callers must ensure that the specified project exists. If the specified project doesn't
+   * exist the call fails with {@link IllegalStateException}.
+   *
+   * @param project project for which the configured code owners backend should be returned
    * @return the {@link CodeOwnersBackend} that should be used
    */
-  public CodeOwnersBackend getBackend() {
-    if (codeOwnersBackend == null) {
+  public CodeOwnersBackend getBackend(Project.NameKey project) {
+    Config pluginConfig = getPluginConfig(project);
+    String backendName = pluginConfig.getString(SECTION_CODE_OWNERS, null, KEY_BACKEND);
+    if (backendName == null) {
+      return getDefaultBackend();
+    }
+    return lookupBackend(backendName)
+        .orElseThrow(
+            () ->
+                new IllegalStateException(
+                    String.format(
+                        "Code owner backend '%s' that is configured in %s.config"
+                            + " (parameter %s.%s) not found",
+                        backendName, pluginName, SECTION_CODE_OWNERS, KEY_BACKEND)));
+  }
+
+  private CodeOwnersBackend getDefaultBackend() {
+    if (defaultCodeOwnersBackend == null) {
+      logger.atInfo().log("reading default backend for plugin %s", pluginName);
       String backendName =
           pluginConfigFactory
               .getFromGerritConfig(pluginName)
               .getString(KEY_BACKEND, FindOwnersBackend.ID);
-      codeOwnersBackend =
+      defaultCodeOwnersBackend =
           lookupBackend(backendName)
               .orElseThrow(
                   () ->
@@ -71,7 +109,25 @@ public class CodeOwnersPluginConfiguration {
                                   + " (parameter plugin.%s.%s) not found",
                               backendName, pluginName, KEY_BACKEND)));
     }
-    return codeOwnersBackend;
+    return defaultCodeOwnersBackend;
+  }
+
+  /**
+   * Reads and returns the config from the {@code code-owners.config} file in {@code
+   * refs/meta/config} branch of the given project.
+   *
+   * @param project the project for which the code owners configurations should be returned
+   * @return the code owners configurations for the given project
+   */
+  private Config getPluginConfig(Project.NameKey project) {
+    logger.atInfo().log("reading plugin config for plugin %s and project %s", pluginName, project);
+    try {
+      return pluginConfigFactory.getProjectPluginConfigWithInheritance(project, pluginName);
+    } catch (NoSuchProjectException e) {
+      throw new IllegalStateException(
+          String.format("cannot code-owners plugin config for non-existing project %s", project),
+          e);
+    }
   }
 
   private Optional<CodeOwnersBackend> lookupBackend(String backendName) {
