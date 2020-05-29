89aff1ef738ffe3b4d25bfa37af1dd3288d40c2d
Edwin Kempin <ekempin@google.com>
2020-05-29 13:23:29 +0200

    Instantiate test API in base class to make it available for all tests
    All acceptance tests will need to use the test API. Instead of
    instantiating the test API in each of the test classes do it once in
    AbstractCodeOwnersTest which is the base class for all acceptance tests.
    
    Use a TestModule for running the tests so that we can bind the test API
    in Guice, since otherwise it cannot be injected.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Ibcd45485449a2b15f880ebaae569f6f1656f25e1

---
+++ java/com/google/gerrit/plugins/codeowners/acceptance/TestModule.java
@@ -0,0 +1,35 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.acceptance;
+
+import com.google.gerrit.plugins.codeowners.Module;
+import com.google.gerrit.plugins.codeowners.acceptance.testsuite.CodeOwnerConfigOperations;
+import com.google.gerrit.plugins.codeowners.acceptance.testsuite.CodeOwnerConfigOperationsImpl;
+import com.google.inject.AbstractModule;
+
+/**
+ * Guice module that makes the extensions of the code-owners plugin and the code owners test API
+ * available during the test execution
+ */
+class TestModule extends AbstractModule {
+  @Override
+  public void configure() {
+    install(new Module());
+
+    // Only add bindings here that are specifically required for tests, in order to keep the Guice
+    // setup in tests as realistic as possible by delegating to the original module.
+    bind(CodeOwnerConfigOperations.class).to(CodeOwnerConfigOperationsImpl.class);
+  }
+}
