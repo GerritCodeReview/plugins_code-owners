779348ab7d62362075ebdd98e68ccb6faaef892a
Edwin Kempin <ekempin@google.com>
2020-05-29 15:31:26 +0200

    Add REST endpoint to get a code owner config for a path in a branch
    The REST endpoints classes (CodeOwnerConfigsInBranchCollection,
    GetCodeOwnerConfigForPathInBranch) use the 'InBranch' identifier,
    because we may want to implement the same REST endpoints for code owner
    configs in changes and then this identifier makes it clear to which REST
    endpoints the classes belong.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I6614f5fd7951ae0e9162127c32ed164e01e21005

---
+++ java/com/google/gerrit/plugins/codeowners/restapi/GetCodeOwnerConfigForPathInBranch.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.restapi;
+
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfig;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwners;
+import com.google.gerrit.plugins.codeowners.restapi.CodeOwnerConfigsInBranchCollection.PathResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.Optional;
+
+/**
+ * REST endpoint that gets the code owner config for an arbitrary path in a branch.
+ *
+ * <p>This REST endpoint handles {@code GET
+ * /projects/<project-name>/branches/<branch-name>/code_owners.config/<path>} requests.
+ *
+ * <p>The path may or may not exist in the branch.
+ */
+@Singleton
+public class GetCodeOwnerConfigForPathInBranch
+    implements RestReadView<CodeOwnerConfigsInBranchCollection.PathResource> {
+  private final CodeOwners codeOwners;
+
+  @Inject
+  GetCodeOwnerConfigForPathInBranch(CodeOwners codeOwners) {
+    this.codeOwners = codeOwners;
+  }
+
+  @Override
+  public Response<CodeOwnerConfigInfo> apply(PathResource rsrc) {
+    Optional<CodeOwnerConfig> codeOwnerConfig = codeOwners.get(rsrc.getCodeOwnerConfigKey());
+    return codeOwnerConfig
+        .map(CodeOwnerConfigJson::format)
+        .map(Response::ok)
+        .orElse(Response.none());
+  }
+}
