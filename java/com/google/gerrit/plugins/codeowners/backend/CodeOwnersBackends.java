9a5775197f57c567a276cf05e2acda6b4d21f2b3
Edwin Kempin <ekempin@google.com>
2020-06-03 12:20:34 +0200

    Add enum for code owners backends
    Having an enum that allows us to iterate over all code owners backends
    will allow us to execute the integration tests once per code owners
    backend (see follow-up change). An enum is needed because the test setup
    is static and we cannot iterate over the registered code owners
    backends, as they are only available after the plugin has been
    installed.
    
    To avoid that we forget to update the enum when a new code owners
    backend is added, we have a test that checks that the enum matches the
    registered code owners backends.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I13d17718b856ea4c3fd99b8dd48a51cf4c9e6359

---
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnersBackends.java
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import com.google.gerrit.plugins.codeowners.backend.findowners.FindOwnersBackend;
+
+/**
+ * Enum of all code owner backend IDs.
+ *
+ * <p>This enum should contain all registered code owner backends.
+ */
+public enum CodeOwnersBackends {
+  FIND_OWNERS(FindOwnersBackend.ID);
+
+  /** The ID under which the code owners backend is registered. */
+  private final String backendId;
+
+  private CodeOwnersBackends(String backendId) {
+    this.backendId = backendId;
+  }
+
+  /** Gets the ID under which the code owners backend is registered. */
+  public String getBackendId() {
+    return backendId;
+  }
+}
