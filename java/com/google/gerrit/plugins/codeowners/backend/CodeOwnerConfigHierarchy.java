63385eb750f071467d95f8a5e5e349b149189698
Edwin Kempin <ekempin@google.com>
2020-06-04 15:38:42 +0200

    Add class that allows us to visit the owner configs that apply for a path
    Code ownerships are inherited from code owner configs in parent folders.
    This means to compute the code owners for a path we must inspect the
    code owner configs of the paths folder and all its parent folders. To
    make the computation of the effective code owners for a path easy add a
    class that iterates over the parent folders and makes a callback for
    each applying code owner config.
    
    Later it will be possible to disable inheriting code owners from parent
    folders, but since this is not implemented yet we always iterate through
    the whole parent folder chain for now.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Id79a29cda08ae4b080d495804db8b269b1dc3374

---
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerConfigHierarchy.java
@@ -0,0 +1,63 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import com.google.gerrit.entities.BranchNameKey;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+/**
+ * Class to visit the code owner configs in a given branch that apply for a given path by following
+ * the path hierarchy from the given path up to the root folder.
+ */
+@Singleton
+public class CodeOwnerConfigHierarchy {
+  private final CodeOwners codeOwners;
+
+  @Inject
+  CodeOwnerConfigHierarchy(CodeOwners codeOwners) {
+    this.codeOwners = codeOwners;
+  }
+
+  /**
+   * Visits the code owner configs in the given branch that apply for the given path by following
+   * the path hierarchy from the given path up to the root folder.
+   *
+   * @param branch project and branch from which the code owner configs should be visited
+   * @param path the path for which the code owner configs should be visited
+   * @param codeOwnerConfigVisitor visitor that should be invoked for the applying code owner
+   *     configs
+   */
+  public void visit(
+      BranchNameKey branch, Path path, Consumer<CodeOwnerConfig> codeOwnerConfigVisitor) {
+    // Next path in which we look for a code owner configuration. We start at the given path and
+    // then go up the parent hierarchy.
+    Path ownerConfigFolder = path;
+
+    // Iterate over the parent code owner configurations.
+    while (ownerConfigFolder != null) {
+      // Read code owner config and invoke the codeOwnerConfigVisitor if the code owner config
+      // exists.
+      codeOwners
+          .get(CodeOwnerConfig.Key.create(branch, ownerConfigFolder))
+          .ifPresent(codeOwnerConfigVisitor);
+
+      // Continue the loop with the next parent folder.
+      ownerConfigFolder = ownerConfigFolder.getParent();
+    }
+  }
+}
