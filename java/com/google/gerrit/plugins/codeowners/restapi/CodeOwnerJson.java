58df1f29b185fb57fa71edbade7e8df444714847
Edwin Kempin <ekempin@google.com>
2020-06-05 14:57:43 +0200

    Add REST endpoint to list the code owners of a file in a branch
    For the initial version of this REST endpoint we only implemented the
    minimal functionality. It's just finding the code owners for a given
    path and returns them. Non-resolveable code owners (emails that cannot
    be resolved to an account or that are ambigious) are omitted from the
    response.
    
    For now we sort the results by account ID to have stable order for the
    tests. This sort order is not documented because we intent to change it
    soon (we intent to order the results by score but we don't have scores
    yet).
    
    For now the REST endpoint doesn't check the visibility of the returned
    accounts. This will be implemented by a follow-up change.
    
    Follow-up changes will also add options to the REST endpoint (e.g. to
    request certain account options, to limit the result etc.).
    
    For the sake of keeping the change small and reviewable this change also
    doesn't add a Java API for listing code owners yet. This is also
    something that will be added by a follow-up change.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: If7a21a7b906f056c29edd2b377a66b2f821c145d

---
+++ java/com/google/gerrit/plugins/codeowners/restapi/CodeOwnerJson.java
@@ -0,0 +1,86 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.restapi;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwner;
+import com.google.gerrit.server.account.AccountDirectory.FillOptions;
+import com.google.gerrit.server.account.AccountLoader;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import java.util.Set;
+
+/** Collection of routines to populate {@link CodeOwnerInfo}s. */
+class CodeOwnerJson {
+  interface Factory {
+    /**
+     * Creates a {@link CodeOwnerJson} instance.
+     *
+     * @param accountOptions account options that control which {@link
+     *     com.google.gerrit.extensions.common.AccountInfo} fields should be populated for the
+     *     accounts in the {@link CodeOwnerInfo}s
+     * @return the created {@link CodeOwnerJson} instance.
+     */
+    CodeOwnerJson create(Set<FillOptions> accountOptions);
+  }
+
+  private final AccountLoader.Factory accountLoaderFactory;
+  private final Set<FillOptions> accountOptions;
+
+  @Inject
+  CodeOwnerJson(
+      AccountLoader.Factory accountLoaderFactory, @Assisted Set<FillOptions> accountOptions) {
+    this.accountLoaderFactory = accountLoaderFactory;
+
+    checkState(!accountOptions.isEmpty(), "account options must not be empty");
+    this.accountOptions = accountOptions;
+  }
+
+  /**
+   * Formats the provided {@link CodeOwner}s as {@link CodeOwnerInfo}s.
+   *
+   * @param codeOwners the code owners that should be formatted as {@link CodeOwnerInfo}s
+   * @return the provided code owners as {@link CodeOwnerInfo}s
+   */
+  ImmutableList<CodeOwnerInfo> format(ImmutableList<CodeOwner> codeOwners)
+      throws PermissionBackendException {
+    AccountLoader accountLoader = accountLoaderFactory.create(accountOptions);
+    ImmutableList<CodeOwnerInfo> codeOwnerInfos =
+        requireNonNull(codeOwners, "codeOwners").stream()
+            .map(codeOwner -> format(accountLoader, codeOwner))
+            .collect(toImmutableList());
+    accountLoader.fill();
+    return codeOwnerInfos;
+  }
+
+  /**
+   * Formats the provided {@link CodeOwner} as {@link CodeOwnerInfo}.
+   *
+   * @param accountLoader the account loader instance that should be used to create the {@link
+   *     com.google.gerrit.extensions.common.AccountInfo} in the code owner info
+   * @param codeOwner the code owner that should be formatted as {@link CodeOwnerInfo}
+   * @return the provided code owner as {@link CodeOwnerInfo}
+   */
+  private static CodeOwnerInfo format(AccountLoader accountLoader, CodeOwner codeOwner) {
+    CodeOwnerInfo info = new CodeOwnerInfo();
+    info.account = accountLoader.get(requireNonNull(codeOwner, "codeOwner").accountId());
+    return info;
+  }
+}
