1f5dc7b0c113d763a907310211c68e5a4b8e3a80
Edwin Kempin <ekempin@google.com>
2020-06-05 14:57:43 +0200

    Add REST endpoint to list the code owners of a file in a branch
    For the initial version of this REST endpoint we only implemented the
    minimal functionality. It's just finding the code owners for a given
    path and returns them. Non-resolveable code owners (emails that cannot
    be resolved to an account or that are ambigious) are omitted from the
    response.
    
    For now we sort the results by account ID to have stable order for the
    tests. This sort order is not documented because we intent to change it
    soon (we intent to order the results by score but we don't have scores
    yet).
    
    For now the REST endpoint doesn't check the visibility of the returned
    accounts. This will be implemented by a follow-up change.
    
    Follow-up changes will also add options to the REST endpoint (e.g. to
    request certain account options, to limit the result etc.).
    
    For the sake of keeping the change small and reviewable this change also
    doesn't add a Java API for listing code owners yet. This is also
    something that will be added by a follow-up change.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: If7a21a7b906f056c29edd2b377a66b2f821c145d

---
+++ java/com/google/gerrit/plugins/codeowners/restapi/GetCodeOwnersForPathInBranch.java
@@ -0,0 +1,86 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.restapi;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestReadView;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwner;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfigHierarchy;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerResolver;
+import com.google.gerrit.plugins.codeowners.restapi.CodeOwnersInBranchCollection.PathResource;
+import com.google.gerrit.server.account.AccountDirectory.FillOptions;
+import com.google.gerrit.server.permissions.PermissionBackendException;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * REST endpoint that gets the code owners for an arbitrary path in a branch.
+ *
+ * <p>This REST endpoint handles {@code GET
+ * /projects/<project-name>/branches/<branch-name>/code_owners/<path>} requests.
+ *
+ * <p>The path may or may not exist in the branch.
+ */
+@Singleton
+public class GetCodeOwnersForPathInBranch
+    implements RestReadView<CodeOwnersInBranchCollection.PathResource> {
+  private final CodeOwnerConfigHierarchy codeOwnerConfigHierarchy;
+  private final CodeOwnerResolver codeOwnerResolver;
+  private final CodeOwnerJson.Factory codeOwnerJsonFactory;
+
+  @Inject
+  GetCodeOwnersForPathInBranch(
+      CodeOwnerConfigHierarchy codeOwnerConfigHierarchy,
+      CodeOwnerResolver codeOwnerResolver,
+      CodeOwnerJson.Factory codeOwnerJsonFactory) {
+    this.codeOwnerConfigHierarchy = codeOwnerConfigHierarchy;
+    this.codeOwnerResolver = codeOwnerResolver;
+    this.codeOwnerJsonFactory = codeOwnerJsonFactory;
+  }
+
+  @Override
+  public Response<List<CodeOwnerInfo>> apply(PathResource rsrc) throws PermissionBackendException {
+    Set<CodeOwner> codeOwners = new HashSet<>();
+    codeOwnerConfigHierarchy.visit(
+        rsrc.getBranch(),
+        rsrc.getPath(),
+        codeOwnerConfig ->
+            codeOwnerConfig
+                .localCodeOwners(codeOwnerConfig.relativize(rsrc.getPath()))
+                .forEach(
+                    codeOwnerReference ->
+                        codeOwnerResolver.resolve(codeOwnerReference).ifPresent(codeOwners::add)));
+    return Response.ok(
+        codeOwnerJsonFactory
+            .create(EnumSet.of(FillOptions.ID))
+            .format(sortCodeOwnersByAccountId(ImmutableSet.copyOf(codeOwners))));
+  }
+
+  private static ImmutableList<CodeOwner> sortCodeOwnersByAccountId(
+      ImmutableSet<CodeOwner> codeOwners) {
+    return codeOwners.stream()
+        .sorted(Comparator.comparing(CodeOwner::accountId))
+        .collect(toImmutableList());
+  }
+}
