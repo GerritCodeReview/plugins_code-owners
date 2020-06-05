657116eddd99ee97388e954351e5488b9826ac9c
Edwin Kempin <ekempin@google.com>
2020-06-05 13:04:39 +0200

    Add a class for resolving code owner references
    Code owner configs contain code owner references that need to be
    resolved to accounts. Add a CodeOwnerResolver class that takes care of
    this. Later we may also check the visibility of the accounts in this
    class, but at this stage we don't care about account visibility yet.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I5e5717250cedcd961ba211872739122078f1840f

---
+++ java/com/google/gerrit/plugins/codeowners/testing/CodeOwnerSubject.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.testing;
+
+import static com.google.common.truth.Truth.assertAbout;
+
+import com.google.common.truth.ComparableSubject;
+import com.google.common.truth.FailureMetadata;
+import com.google.common.truth.Subject;
+import com.google.gerrit.entities.Account;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwner;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfig;
+
+/** {@link Subject} for doing assertions on {@link CodeOwner}s. */
+public class CodeOwnerSubject extends Subject {
+  /**
+   * Starts fluent chain to do assertions on a {@link CodeOwnerConfig}.
+   *
+   * @param codeOwner the code owner config on which assertions should be done
+   * @return the created {@link CodeOwnerConfigSubject}
+   */
+  public static CodeOwnerSubject assertThat(CodeOwner codeOwner) {
+    return assertAbout(CodeOwnerSubject::new).that(codeOwner);
+  }
+
+  private final CodeOwner codeOwner;
+
+  private CodeOwnerSubject(FailureMetadata metadata, CodeOwner codeOwner) {
+    super(metadata, codeOwner);
+    this.codeOwner = codeOwner;
+  }
+
+  /**
+   * Returns a {@link ComparableSubject} for the account ID of the code owner.
+   *
+   * @return {@link ComparableSubject} for the account ID of the code owner
+   */
+  public ComparableSubject<Account.Id> hasAccountIdThat() {
+    return check("accountId()").that(codeOwner().accountId());
+  }
+
+  private CodeOwner codeOwner() {
+    isNotNull();
+    return codeOwner;
+  }
+}
