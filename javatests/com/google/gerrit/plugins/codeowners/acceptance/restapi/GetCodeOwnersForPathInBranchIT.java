e27e76a23a53c5ffc059e9a0cef13fca6526e50e
Edwin Kempin <ekempin@google.com>
2020-06-05 14:57:43 +0200

    Add REST endpoint to list the code owners of a file in a branch
    For the initial version of this REST endpoint we only implemented the
    minimal functionality. It's just finding the code owners for a given
    path and returns them. Non-resolveable code owners (emails that cannot
    be resolved to an account or that are ambigious) are omitted from the
    response.
    
    For now we sort the results by account ID to have stable order for the
    tests. This sort order is not documented because we intent to change it
    soon (we intent to order the results by score but we don't have scores
    yet).
    
    For now the REST endpoint doesn't check the visibility of the returned
    accounts. This will be implemented by a follow-up change.
    
    Follow-up changes will also add options to the REST endpoint (e.g. to
    request certain account options, to limit the result etc.).
    
    For the sake of keeping the change small and reviewable this change also
    doesn't add a Java API for listing code owners yet. This is also
    something that will be added by a follow-up change.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: If7a21a7b906f056c29edd2b377a66b2f821c145d

---
+++ javatests/com/google/gerrit/plugins/codeowners/acceptance/restapi/GetCodeOwnersForPathInBranchIT.java
@@ -0,0 +1,123 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.acceptance.restapi;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.plugins.codeowners.testing.CodeOwnerInfoIterableSubject.assertThat;
+
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.acceptance.TestAccount;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.restapi.IdString;
+import com.google.gerrit.plugins.codeowners.acceptance.AbstractCodeOwnersIT;
+import com.google.gerrit.plugins.codeowners.restapi.CodeOwnerInfo;
+import com.google.gson.reflect.TypeToken;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * Acceptance test for the {@link
+ * com.google.gerrit.plugins.codeowners.restapi.GetCodeOwnersForPathInBranch} REST endpoint.
+ */
+public class GetCodeOwnersForPathInBranchIT extends AbstractCodeOwnersIT {
+  @Test
+  public void getCodeOwnersWhenNoCodeOwnerConfigsExist() throws Exception {
+    assertThat(getCodeOwnersViaRest(project, "master", "/foo/bar/baz.md")).isEmpty();
+  }
+
+  @Test
+  public void getCodeOwnersUnrelatedCodeOwnerConfigHasNoEffect() throws Exception {
+    codeOwnerConfigOperations
+        .newCodeOwnerConfig()
+        .project(project)
+        .branch("master")
+        .folderPath("/abc/def/")
+        .addCodeOwnerEmail(admin.email())
+        .addCodeOwnerEmail(user.email())
+        .create();
+    assertThat(getCodeOwnersViaRest(project, "master", "/foo/bar/baz.md")).isEmpty();
+  }
+
+  @Test
+  public void getCodeOwnersForAbsolutePath() throws Exception {
+    testGetCodeOwners(true);
+  }
+
+  @Test
+  public void getCodeOwnersForNonAbsolutePath() throws Exception {
+    testGetCodeOwners(false);
+  }
+
+  private void testGetCodeOwners(boolean useAbsolutePath) throws Exception {
+    TestAccount user2 = accountCreator.user2();
+
+    codeOwnerConfigOperations
+        .newCodeOwnerConfig()
+        .project(project)
+        .branch("master")
+        .folderPath("/")
+        .addCodeOwnerEmail(admin.email())
+        .create();
+
+    codeOwnerConfigOperations
+        .newCodeOwnerConfig()
+        .project(project)
+        .branch("master")
+        .folderPath("/foo/")
+        .addCodeOwnerEmail(user.email())
+        .create();
+
+    codeOwnerConfigOperations
+        .newCodeOwnerConfig()
+        .project(project)
+        .branch("master")
+        .folderPath("/foo/bar/")
+        .addCodeOwnerEmail(user2.email())
+        .create();
+
+    assertThat(
+            getCodeOwnersViaRest(
+                project, "master", useAbsolutePath ? "/foo/bar/baz.md" : "foo/bar/baz.md"))
+        .hasAccountIdsThat()
+        .containsExactly(admin.id(), user.id(), user2.id())
+        .inOrder();
+  }
+
+  @Test
+  public void getCodeOwnerConfigForInvalidPath() throws Exception {
+    RestResponse r =
+        adminRestSession.get(
+            String.format(
+                "/projects/%s/branches/%s/code_owners/%s",
+                IdString.fromDecoded(project.get()),
+                IdString.fromDecoded("master"),
+                IdString.fromDecoded("\0")));
+    r.assertBadRequest();
+    assertThat(r.getEntityContent()).contains("Nul character not allowed");
+  }
+
+  private List<CodeOwnerInfo> getCodeOwnersViaRest(
+      Project.NameKey project, String branchName, String path) throws Exception {
+    RestResponse r =
+        adminRestSession.get(
+            String.format(
+                "/projects/%s/branches/%s/code_owners/%s",
+                IdString.fromDecoded(project.get()),
+                IdString.fromDecoded(branchName),
+                IdString.fromDecoded(path)));
+    r.assertOK();
+    return newGson().fromJson(r.getReader(), new TypeToken<List<CodeOwnerInfo>>() {}.getType());
+  }
+}
