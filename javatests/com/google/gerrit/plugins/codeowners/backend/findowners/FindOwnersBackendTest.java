b2a9175da93ccee4ffd5ab7aef5a8593fbf7f9c3
Edwin Kempin <ekempin@google.com>
2020-06-16 11:26:02 +0200

    Add a CodeOwnersBackend for the proto syntax
    The implementation of the new backend is largely the same as the
    existing FindOwnersBackend. The only difference is the name of the code
    owner config files and the used CodeOwnerConfigParser. To avoid code
    duplication implement an abstract backend that stores code owner configs
    in files inside the corresponding folders in the source branch.
    
    For testing CodeOwnersBackend implementations we now also have a base
    class that implements the common tests, so that they do not need to be
    duplicated.
    
    The tests for deleting code owner configs are specific for the
    FindOwnersBackend since the ProtoBackend doesn't delete empty code owner
    configs. We may change this in a follow-up change to make the backends
    behave more consistent.
    
    All acceptance tests are automatically executed against the new backend
    as well.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Ie52cd9cca6330aa1698174652bf33a7ca660e16b

--- javatests/com/google/gerrit/plugins/codeowners/backend/findowners/FindOwnersBackendTest.java
+++ javatests/com/google/gerrit/plugins/codeowners/backend/findowners/FindOwnersBackendTest.java
@@ -16,184 +16,35 @@ package com.google.gerrit.plugins.codeowners.backend.findowners;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth8.assertThat;
-import static com.google.gerrit.plugins.codeowners.testing.CodeOwnerConfigSubject.assertThat;
-import static com.google.gerrit.testing.GerritJUnit.assertThrows;
 
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
 import com.google.gerrit.common.Nullable;
-import com.google.gerrit.plugins.codeowners.acceptance.AbstractCodeOwnersTest;
+import com.google.gerrit.plugins.codeowners.backend.AbstractFileBasedCodeOwnersBackend;
+import com.google.gerrit.plugins.codeowners.backend.AbstractFileBasedCodeOwnersBackendTest;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfig;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfigParser;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfigUpdate;
-import com.google.gerrit.plugins.codeowners.backend.CodeOwnerReference;
-import com.google.gerrit.plugins.codeowners.testing.backend.BackendTestUtil;
 import com.google.gerrit.server.IdentifiedUser;
 import java.util.Optional;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.junit.Before;
 import org.junit.Test;
 
 /** Tests for {@link FindOwnersBackend}. */
-public class FindOwnersBackendTest extends AbstractCodeOwnersTest {
-  private BackendTestUtil backendTestUtil;
-  private FindOwnersBackend findOwnersBackend;
-
-  @Before
-  public void setUpCodeOwnersPlugin() throws Exception {
-    backendTestUtil =
-        plugin
-            .getSysInjector()
-            .getInstance(BackendTestUtil.Factory.class)
-            .create(
-                FindOwnersBackend.CODE_OWNER_CONFIG_FILE_NAME,
-                plugin.getSysInjector().getInstance(FindOwnersCodeOwnerConfigParser.class));
-    findOwnersBackend = plugin.getSysInjector().getInstance(FindOwnersBackend.class);
+public class FindOwnersBackendTest extends AbstractFileBasedCodeOwnersBackendTest {
+  @Override
+  protected Class<? extends AbstractFileBasedCodeOwnersBackend> getBackendClass() {
+    return FindOwnersBackend.class;
   }
 
-  @Test
-  public void getNonExistingCodeOwnerConfig() throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey =
-        CodeOwnerConfig.Key.create(project, "master", "/non-existing/");
-    assertThat(findOwnersBackend.getCodeOwnerConfig(codeOwnerConfigKey)).isEmpty();
+  @Override
+  protected String getFileName() {
+    return FindOwnersBackend.CODE_OWNER_CONFIG_FILE_NAME;
   }
 
-  @Test
-  public void getCodeOwnerConfigFromNonExistingBranch() throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey =
-        CodeOwnerConfig.Key.create(project, "non-existing", "/");
-    assertThat(findOwnersBackend.getCodeOwnerConfig(codeOwnerConfigKey)).isEmpty();
-  }
-
-  @Test
-  public void getCodeOwnerConfig() throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey = CodeOwnerConfig.Key.create(project, "master", "/");
-    CodeOwnerConfig codeOwnerConfigInRepository =
-        CodeOwnerConfig.builder(codeOwnerConfigKey).addCodeOwnerEmail(admin.email()).build();
-    backendTestUtil.writeCodeOwnerConfig(codeOwnerConfigInRepository);
-
-    Optional<CodeOwnerConfig> codeOwnerConfig =
-        findOwnersBackend.getCodeOwnerConfig(codeOwnerConfigKey);
-    assertThat(codeOwnerConfig).isPresent();
-    assertThat(codeOwnerConfig.get()).isEqualTo(codeOwnerConfigInRepository);
-  }
-
-  @Test
-  public void createCodeOwnerConfigInitiatedByServer() throws Exception {
-    testCreateCodeOwnerConfig(null);
-  }
-
-  @Test
-  public void createCodeOwnerConfigInitiatedByUser() throws Exception {
-    testCreateCodeOwnerConfig(identifiedUserFactory.create(user.id()));
-  }
-
-  private void testCreateCodeOwnerConfig(@Nullable IdentifiedUser currentUser) throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey = CodeOwnerConfig.Key.create(project, "master", "/");
-
-    try (Repository repo = repoManager.openRepository(project)) {
-      // Remember head for later assertions.
-      RevCommit origHead = getHead(repo, codeOwnerConfigKey.ref());
-
-      // Create the code owner config.
-      Optional<CodeOwnerConfig> codeOwnerConfig =
-          findOwnersBackend.upsertCodeOwnerConfig(
-              codeOwnerConfigKey,
-              CodeOwnerConfigUpdate.builder()
-                  .setCodeOwnerModification(
-                      codeOwners ->
-                          Sets.union(
-                              codeOwners,
-                              ImmutableSet.of(CodeOwnerReference.create(admin.email()))))
-                  .build(),
-              currentUser);
-      assertThat(codeOwnerConfig).isPresent();
-      assertThat(codeOwnerConfig.get()).hasCodeOwnersEmailsThat().containsExactly(admin.email());
-
-      // Check the metadata of the created commit.
-      RevCommit newHead = getHead(repo, codeOwnerConfigKey.ref());
-      assertThat(newHead.getParent(0)).isEqualTo(origHead);
-      assertThat(newHead.getShortMessage()).isEqualTo("Create code owner config");
-
-      // Check author identity.
-      if (currentUser != null) {
-        assertThat(newHead.getAuthorIdent().getEmailAddress())
-            .isEqualTo(currentUser.getAccount().preferredEmail());
-      } else {
-        assertThat(newHead.getAuthorIdent().getEmailAddress())
-            .isEqualTo(serverIdent.get().getEmailAddress());
-        assertThat(newHead.getCommitterIdent()).isEqualTo(newHead.getAuthorIdent());
-      }
-
-      // Check committer identity.
-      assertThat(newHead.getCommitterIdent().getEmailAddress())
-          .isEqualTo(serverIdent.get().getEmailAddress());
-      assertThat(newHead.getCommitterIdent().getTimeZone())
-          .isEqualTo(newHead.getAuthorIdent().getTimeZone());
-      assertThat(newHead.getCommitterIdent().getWhen())
-          .isEqualTo(newHead.getAuthorIdent().getWhen());
-    }
-  }
-
-  @Test
-  public void updateCodeOwnerConfigInitiatedByServer() throws Exception {
-    testUpdateCodeOwnerConfig(null);
-  }
-
-  @Test
-  public void updateCodeOwnerConfigInitiatedByUser() throws Exception {
-    testUpdateCodeOwnerConfig(identifiedUserFactory.create(user.id()));
-  }
-
-  private void testUpdateCodeOwnerConfig(@Nullable IdentifiedUser currentUser) throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey = CodeOwnerConfig.Key.create(project, "master", "/");
-    CodeOwnerConfig codeOwnerConfigInRepository =
-        CodeOwnerConfig.builder(codeOwnerConfigKey).addCodeOwnerEmail(admin.email()).build();
-    backendTestUtil.writeCodeOwnerConfig(codeOwnerConfigInRepository);
-
-    try (Repository repo = repoManager.openRepository(project)) {
-      // Remember head for later assertions.
-      RevCommit origHead = getHead(repo, codeOwnerConfigKey.ref());
-
-      // Create the code owner config.
-      Optional<CodeOwnerConfig> codeOwnerConfig =
-          findOwnersBackend.upsertCodeOwnerConfig(
-              codeOwnerConfigKey,
-              CodeOwnerConfigUpdate.builder()
-                  .setCodeOwnerModification(
-                      codeOwners ->
-                          Sets.union(
-                              codeOwners, ImmutableSet.of(CodeOwnerReference.create(user.email()))))
-                  .build(),
-              currentUser);
-      assertThat(codeOwnerConfig).isPresent();
-      assertThat(codeOwnerConfig.get())
-          .hasCodeOwnersEmailsThat()
-          .containsExactly(admin.email(), user.email());
-
-      // Check the metadata of the created commit.
-      RevCommit newHead = getHead(repo, codeOwnerConfigKey.ref());
-      assertThat(newHead.getParent(0)).isEqualTo(origHead);
-      assertThat(newHead.getShortMessage()).isEqualTo("Update code owner config");
-
-      // Check author identity.
-      if (currentUser != null) {
-        assertThat(newHead.getAuthorIdent().getEmailAddress())
-            .isEqualTo(currentUser.getAccount().preferredEmail());
-      } else {
-        assertThat(newHead.getAuthorIdent().getEmailAddress())
-            .isEqualTo(serverIdent.get().getEmailAddress());
-        assertThat(newHead.getCommitterIdent()).isEqualTo(newHead.getAuthorIdent());
-      }
-
-      // Check committer identity.
-      assertThat(newHead.getCommitterIdent().getEmailAddress())
-          .isEqualTo(serverIdent.get().getEmailAddress());
-      assertThat(newHead.getCommitterIdent().getTimeZone())
-          .isEqualTo(newHead.getAuthorIdent().getTimeZone());
-      assertThat(newHead.getCommitterIdent().getWhen())
-          .isEqualTo(newHead.getAuthorIdent().getWhen());
-    }
+  @Override
+  protected Class<? extends CodeOwnerConfigParser> getParserClass() {
+    return FindOwnersCodeOwnerConfigParser.class;
   }
 
   @Test
@@ -219,7 +70,7 @@ public class FindOwnersBackendTest extends AbstractCodeOwnersTest {
 
       // Create the code owner config.
       Optional<CodeOwnerConfig> codeOwnerConfig =
-          findOwnersBackend.upsertCodeOwnerConfig(
+          codeOwnersBackend.upsertCodeOwnerConfig(
               codeOwnerConfigKey,
               CodeOwnerConfigUpdate.builder()
                   .setCodeOwnerModification(codeOwners -> ImmutableSet.of())
@@ -249,51 +100,6 @@ public class FindOwnersBackendTest extends AbstractCodeOwnersTest {
           .isEqualTo(newHead.getAuthorIdent().getTimeZone());
       assertThat(newHead.getCommitterIdent().getWhen())
           .isEqualTo(newHead.getAuthorIdent().getWhen());
-    }
-  }
-
-  @Test
-  public void noOpCodeOwnerConfigUpdate() throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey = CodeOwnerConfig.Key.create(project, "master", "/");
-    CodeOwnerConfig codeOwnerConfigInRepository =
-        CodeOwnerConfig.builder(codeOwnerConfigKey).addCodeOwnerEmail(admin.email()).build();
-    backendTestUtil.writeCodeOwnerConfig(codeOwnerConfigInRepository);
-
-    try (Repository repo = repoManager.openRepository(project)) {
-      // Remember head for later assertions.
-      RevCommit origHead = getHead(repo, codeOwnerConfigKey.ref());
-
-      // Create the code owner config.
-      Optional<CodeOwnerConfig> codeOwnerConfig =
-          findOwnersBackend.upsertCodeOwnerConfig(
-              codeOwnerConfigKey, CodeOwnerConfigUpdate.builder().build(), null);
-      assertThat(codeOwnerConfig).isPresent();
-      assertThat(codeOwnerConfig.get()).hasCodeOwnersEmailsThat().containsExactly(admin.email());
-
-      // Check that no commit was created.
-      assertThat(getHead(repo, codeOwnerConfigKey.ref())).isEqualTo(origHead);
-    }
-  }
-
-  @Test
-  public void cannotUpdateCodeOwnerConfigInNonExistingBranch() throws Exception {
-    CodeOwnerConfig.Key codeOwnerConfigKey =
-        CodeOwnerConfig.Key.create(project, "non-existing", "/");
-
-    try (Repository repo = repoManager.openRepository(project)) {
-      // Create the code owner config.
-      IllegalStateException exception =
-          assertThrows(
-              IllegalStateException.class,
-              () ->
-                  findOwnersBackend.upsertCodeOwnerConfig(
-                      codeOwnerConfigKey, CodeOwnerConfigUpdate.builder().build(), null));
-      assertThat(exception)
-          .hasMessageThat()
-          .isEqualTo(String.format("branch %s does not exist", codeOwnerConfigKey.ref()));
-
-      // Check that the branch was not created.
-      assertThat(repo.exactRef(codeOwnerConfigKey.ref())).isNull();
     }
   }
 }
