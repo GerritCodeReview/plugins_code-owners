9750958ec9a6043bcb71bbb6573b21981111c285
Edwin Kempin <ekempin@google.com>
2020-06-22 17:06:50 +0200

    CodeOwnersPluginConfiguration: Fix exception message
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I4441fae072ca394512a34e937ab2d171ec875ac5

--- javatests/com/google/gerrit/plugins/codeowners/CodeOwnersPluginConfigurationTest.java
+++ javatests/com/google/gerrit/plugins/codeowners/CodeOwnersPluginConfigurationTest.java
@@ -30,6 +30,7 @@ import com.google.gerrit.plugins.codeowners.acceptance.AbstractCodeOwnersTest;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfig;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfigUpdate;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnersBackend;
+import com.google.gerrit.plugins.codeowners.backend.PathExpressionMatcher;
 import com.google.gerrit.plugins.codeowners.backend.findowners.FindOwnersBackend;
 import com.google.gerrit.server.IdentifiedUser;
 import com.google.inject.Inject;
@@ -70,7 +71,7 @@ public class CodeOwnersPluginConfigurationTest extends AbstractCodeOwnersTest {
     assertThat(exception)
         .hasMessageThat()
         .isEqualTo(
-            "cannot code-owners plugin config for non-existing project non-existing-project");
+            "cannot get code-owners plugin config for non-existing project non-existing-project");
   }
 
   @Test
@@ -294,6 +295,11 @@ public class CodeOwnersPluginConfigurationTest extends AbstractCodeOwnersTest {
         CodeOwnerConfigUpdate codeOwnerConfigUpdate,
         @Nullable IdentifiedUser currentUser) {
       throw new UnsupportedOperationException("not implemented");
+    }
+
+    @Override
+    public PathExpressionMatcher getPathExpressionMatcher() {
+      return PathExpressionMatcher.notSupportedBy(ID);
     }
   }
 }
