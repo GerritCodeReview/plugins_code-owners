c53df640a80a02b3ccb5f1f067e715343dd4baae
Edwin Kempin <ekempin@google.com>
2020-07-14 14:13:22 +0200

    Add class to compute the code owner statuses for the files in a change
    We need the code owner statuses for the files in a change for 2
    purposes:
    
    1. to implement a REST endpoint that returns them to the UI so that they
       can be shown to the user
    2. to check whether a change is submittable
    
    In order for a change to be submittable every file, or rather touched
    path, in a change needs to be approved by a code owner:
    
    * New file:
      requires approval on the new path
    
    * Modified file:
      requires approval on the old/new path (old path == new path)
    
    * Deleted file:
      requires approval on the old path
    
    * Renamed file:
      requires approval on the old and new path (equivalent to delete old
      path + add new path)
    
    * Copied file:
      requires approval on the new path (an approval on the old path is not
      needed since the file at this path was not touched)
    
    For every touched path the following code owner statuses are possible:
    
    * INSUFFICIENT_REVIEWERS:
      The path needs an owner approval, but none of its code owners is
      currently a reviewer of the change.
    
    * PENDING:
      A code owner of this path has been added as reviewer, but no code
      owner approval for this path has been given yet.
    
    * APPROVED:
      The path has been approved by a code owner or a code owners override
      is present (code owner overrides will be implemented only later).
    
    The new CodeOwnerApprovalCheck computes the code owner statuses for all
    files/paths that were changed in a given revision. For this computation
    it iterates over the touched paths to look up the code owners. The code
    owners are then matched against the reviewers/approvers on the change.
    
    In order for a path to be approved we require an explicit vote on the
    required label. Later we will also consider an implicit approval from
    the patch set uploader, if the patch set uploader is a code owner (but
    this will only be implemented in a follow-up change, for now we have a
    TODO for this in CodeOwnerApprovalCheck).
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I95ab49f4cd04633046c9072c4083b797ba6b59ee

---
+++ java/com/google/gerrit/plugins/codeowners/testing/FileCodeOwnerStatusSubject.java
@@ -0,0 +1,71 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.testing;
+
+import static com.google.gerrit.plugins.codeowners.testing.ChangedFileSubject.changedFiles;
+import static com.google.gerrit.truth.OptionalSubject.optionals;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.truth.FailureMetadata;
+import com.google.common.truth.Subject;
+import com.google.gerrit.plugins.codeowners.backend.FileCodeOwnerStatus;
+import com.google.gerrit.truth.ListSubject;
+import com.google.gerrit.truth.OptionalSubject;
+import java.util.Set;
+
+/** {@link Subject} for doing assertions on {@link FileCodeOwnerStatus}es. */
+public class FileCodeOwnerStatusSubject extends Subject {
+  public static ListSubject<FileCodeOwnerStatusSubject, FileCodeOwnerStatus> assertThatSet(
+      Set<FileCodeOwnerStatus> fileCodeOwnerStatuses) {
+    return ListSubject.assertThat(
+        ImmutableList.copyOf(fileCodeOwnerStatuses), fileCodeOwnerStatuses());
+  }
+
+  private static Factory<FileCodeOwnerStatusSubject, FileCodeOwnerStatus> fileCodeOwnerStatuses() {
+    return FileCodeOwnerStatusSubject::new;
+  }
+
+  private final FileCodeOwnerStatus fileCodeOwnerStatus;
+
+  private FileCodeOwnerStatusSubject(
+      FailureMetadata metadata, FileCodeOwnerStatus fileCodeOwnerStatus) {
+    super(metadata, fileCodeOwnerStatus);
+    this.fileCodeOwnerStatus = fileCodeOwnerStatus;
+  }
+
+  /** Returns a subject for the changed file. */
+  public ChangedFileSubject hasChangedFile() {
+    return check("changedFile()").about(changedFiles()).that(fileCodeOwnerStatus().changedFile());
+  }
+
+  /** Returns an {@link OptionalSubject} for the code owners status of the new path. */
+  public OptionalSubject<Subject, ?> hasNewPathCodeOwnerStatus() {
+    return check("newPathCodeOwnerStatus()")
+        .about(optionals())
+        .that(fileCodeOwnerStatus().newPathCodeOwnerStatus());
+  }
+
+  /** Returns an {@link OptionalSubject} for the code owners status of the old path. */
+  public OptionalSubject<Subject, ?> hasOldPathCodeOwnerStatus() {
+    return check("oldPathCodeOwnerStatus()")
+        .about(optionals())
+        .that(fileCodeOwnerStatus().oldPathCodeOwnerStatus());
+  }
+
+  private FileCodeOwnerStatus fileCodeOwnerStatus() {
+    isNotNull();
+    return fileCodeOwnerStatus;
+  }
+}
