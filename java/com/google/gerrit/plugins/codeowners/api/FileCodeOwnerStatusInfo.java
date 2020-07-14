2d4509e2b2364c024bbb261a68fae8dbd50de3b8
Edwin Kempin <ekempin@google.com>
2020-07-14 14:15:24 +0200

    Add REST endpoint to get the code owner statuses for the files in a change
    This REST endpoint returns the code owner statuses for the the files in
    a change. Depnding on the change type changed files can have a new
    and/or old path (e.g. a rename has a new and old path, a deletion only
    has an old path) for which code owner approvals are required. To make
    callers aware of this the returned file code owner statuses contain code
    owner statuses for the old and new path, if they are present.
    
    A path in a change can have the following code owner statuses:
    
    * INSUFFICIENT_REVIEWERS:
      The path needs a code owner approval, but none of its code owners is
      currently a reviewer of the change.
    
    * PENDING:
      A code owner of this path has been added as reviewer, but no code
      owner approval for this path has been given yet.
    
    * APPROVED:
      The path has been approved by a code owner or a code owners override
      is present.
    
    The GetCodeOwnerStatuses REST endpoint is offered on change level,
    rather than on revision level because:
    
    * The code owner statuses are based on votes and we always show the
      current votes on the change screen even if users view an old revision
      / patch set (it would be confusing for users to see the current votes,
      but non-matching code owner statuses).
    
    * The code owner statuses for old revisions are never relevant and may
      confuse users (e.g. the code owner status 'PENDING' means that we are
      waiting for the code owner approval of a current reviewer, but the UI
      doesn’t allow reviewers to vote on non-current revisions, so that this
      approval can never happen).
    
    This means the frontend can only show owner statuses for the current
    patch set.
    
    In addition there is a risk for races if new patch sets are uploaded:
    
    1. The frontend loads the change when patch set N is current.
    2. A user uploads a new patch set N+1.
    3. The frontend loads the owner statuses which are returned for patch
       set N+1.
    
    To allow the frontend to detect this situation, the response includes
    the patch set for which the owner statuses were returned (see field
    patch_set_number). The frontend can then show a warning or do a refresh
    when the patch sets mismatch.
    
    To avoid this issue we considered to return the owner statuses as part
    of ChangeInfo (plugins field), but we know from the checks plugin that
    this is not working well:
    
    * the code owner statuses should not be returned by default, but only
      when requested (most callers don’t need the code owner statuses)
    
    * this means the caller would need to set an option on the request to
      include the code owner statuses
    
    * the frontend loads the ChangeInfo before it knows which plugins are
      active, hence at the moment when the ChangeInfo is loaded the frontend
      doesn’t know yet that it needs the code owner statuses
    
    We also considered returning the file code owner statuses as map of file
    path to file code owner status, but this doesn't work well when a file
    is renamed, as in this case the files has 2 code owner statuses, one for
    the old path and one for the new path. We could have returned both path
    in the map but then the client would have needed to figure out which
    2 paths form a file rename.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I6c449448e9d541a6d36ea9ac4805248a6cd6ed60

---
+++ java/com/google/gerrit/plugins/codeowners/api/FileCodeOwnerStatusInfo.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.api;
+
+import com.google.gerrit.extensions.common.ChangeType;
+
+/** JSON entity that describes the code owner status for a file that was touched in a change. */
+public class FileCodeOwnerStatusInfo {
+  /**
+   * The type of the file modification.
+   *
+   * <p>Not set if {@link ChangeType#MODIFIED}.
+   */
+  public ChangeType changeType;
+
+  /**
+   * Code owner status for the old path of the file.
+   *
+   * <p>Only set if the file was deleted or renamed.
+   */
+  public PathCodeOwnerStatusInfo oldPathCodeOwnerStatus;
+
+  /**
+   * Code owner status for the new path of the file.
+   *
+   * <p>Not set if the file was deleted.
+   */
+  public PathCodeOwnerStatusInfo newPathCodeOwnerStatus;
+}
---
+++ java/com/google/gerrit/plugins/codeowners/restapi/CodeOwnerStatusInfoJson.java
@@ -0,0 +1,132 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.restapi;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.Comparator.comparing;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.extensions.common.ChangeType;
+import com.google.gerrit.plugins.codeowners.JgitPath;
+import com.google.gerrit.plugins.codeowners.api.CodeOwnerStatusInfo;
+import com.google.gerrit.plugins.codeowners.api.FileCodeOwnerStatusInfo;
+import com.google.gerrit.plugins.codeowners.api.PathCodeOwnerStatusInfo;
+import com.google.gerrit.plugins.codeowners.backend.FileCodeOwnerStatus;
+import com.google.gerrit.plugins.codeowners.backend.FileCodeOwnerStatus.PathCodeOwnerStatus;
+import java.util.Comparator;
+import org.eclipse.jgit.diff.DiffEntry;
+
+/** Collection of routines to populate {@link CodeOwnerStatusInfo}. */
+public class CodeOwnerStatusInfoJson {
+  /** Comparator that sorts {@link FileCodeOwnerStatus} by new path and then old path. */
+  private static final Comparator<FileCodeOwnerStatus> FILE_CODE_OWNER_STATUS_COMPARATOR =
+      comparing(
+          (FileCodeOwnerStatus a) -> {
+            if (a.newPathCodeOwnerStatus().isPresent()) {
+              return a.newPathCodeOwnerStatus().get().path().toString();
+            }
+            if (a.oldPathCodeOwnerStatus().isPresent()) {
+              return a.oldPathCodeOwnerStatus().get().path().toString();
+            }
+            // should never happen since one of new path and old path is always set
+            return "";
+          });
+
+  private static final ImmutableMap<DiffEntry.ChangeType, ChangeType> CHANGE_TYPE =
+      Maps.immutableEnumMap(
+          new ImmutableMap.Builder<DiffEntry.ChangeType, ChangeType>()
+              .put(DiffEntry.ChangeType.ADD, ChangeType.ADDED)
+              .put(DiffEntry.ChangeType.MODIFY, ChangeType.MODIFIED)
+              .put(DiffEntry.ChangeType.DELETE, ChangeType.DELETED)
+              .put(DiffEntry.ChangeType.RENAME, ChangeType.RENAMED)
+              .put(DiffEntry.ChangeType.COPY, ChangeType.COPIED)
+              .build());
+
+  /**
+   * Formats a {@link CodeOwnerStatusInfo} from the provided file code owner statuses.
+   *
+   * @param patchSetId the ID of the patch set for which the file code owner statuses were computed
+   * @param fileCodeOwnerStatuses the {@link FileCodeOwnerStatus}es that should be set in the {@link
+   *     CodeOwnerStatusInfo}
+   * @return the created {@link CodeOwnerStatusInfo}
+   */
+  public static CodeOwnerStatusInfo format(
+      PatchSet.Id patchSetId, ImmutableSet<FileCodeOwnerStatus> fileCodeOwnerStatuses) {
+    requireNonNull(patchSetId, "patchSetId");
+    requireNonNull(fileCodeOwnerStatuses, "fileCodeOwnerStatuses");
+    CodeOwnerStatusInfo info = new CodeOwnerStatusInfo();
+    info.patchSetNumber = patchSetId.get();
+    info.fileCodeOwnerStatuses =
+        fileCodeOwnerStatuses.stream()
+            .sorted(FILE_CODE_OWNER_STATUS_COMPARATOR)
+            .map(CodeOwnerStatusInfoJson::format)
+            .collect(toImmutableList());
+    return info;
+  }
+
+  /**
+   * Formats the provided {@link FileCodeOwnerStatus} as {@link FileCodeOwnerStatusInfo}.
+   *
+   * @param fileCodeOwnerStatus the {@link FileCodeOwnerStatus} that should be formatted as {@link
+   *     FileCodeOwnerStatusInfo}
+   * @return the provided {@link FileCodeOwnerStatus} as {@link FileCodeOwnerStatusInfo}
+   */
+  @VisibleForTesting
+  static FileCodeOwnerStatusInfo format(FileCodeOwnerStatus fileCodeOwnerStatus) {
+    requireNonNull(fileCodeOwnerStatus, "fileCodeOwnerStatus");
+    FileCodeOwnerStatusInfo info = new FileCodeOwnerStatusInfo();
+    info.changeType =
+        fileCodeOwnerStatus.changedFile().changeType() != DiffEntry.ChangeType.MODIFY
+            ? CHANGE_TYPE.get(fileCodeOwnerStatus.changedFile().changeType())
+            : null;
+    fileCodeOwnerStatus
+        .oldPathCodeOwnerStatus()
+        .ifPresent(
+            oldPathCodeOwnerStatus -> info.oldPathCodeOwnerStatus = format(oldPathCodeOwnerStatus));
+    fileCodeOwnerStatus
+        .newPathCodeOwnerStatus()
+        .ifPresent(
+            newPathCodeOwnerStatus -> info.newPathCodeOwnerStatus = format(newPathCodeOwnerStatus));
+    return info;
+  }
+
+  /**
+   * Formats the provided {@link PathCodeOwnerStatus} as {@link PathCodeOwnerStatusInfo}.
+   *
+   * @param pathCodeOwnerStatus the {@link PathCodeOwnerStatus} that should be formatted as {@link
+   *     PathCodeOwnerStatusInfo}
+   * @return the provided {@link PathCodeOwnerStatus} as {@link PathCodeOwnerStatusInfo}
+   */
+  @VisibleForTesting
+  static PathCodeOwnerStatusInfo format(PathCodeOwnerStatus pathCodeOwnerStatus) {
+    requireNonNull(pathCodeOwnerStatus, "pathCodeOwnerStatus");
+    PathCodeOwnerStatusInfo info = new PathCodeOwnerStatusInfo();
+    info.path = JgitPath.of(pathCodeOwnerStatus.path()).get();
+    info.codeOwnerStatus = pathCodeOwnerStatus.codeOwnerStatus();
+    return info;
+  }
+
+  /**
+   * Private constructor to prevent instantiation of this class.
+   *
+   * <p>The class only contains static methods, hence the class never needs to be instantiated.
+   */
+  private CodeOwnerStatusInfoJson() {}
+}
