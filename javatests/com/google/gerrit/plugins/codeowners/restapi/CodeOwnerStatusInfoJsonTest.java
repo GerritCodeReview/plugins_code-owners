bc357fccda9589532e775b8ee7413db23a379144
Edwin Kempin <ekempin@google.com>
2020-07-14 14:15:24 +0200

    Add REST endpoint to get the code owner statuses for the files in a change
    This REST endpoint returns the code owner statuses for the the files in
    a change. Depnding on the change type changed files can have a new
    and/or old path (e.g. a rename has a new and old path, a deletion only
    has an old path) for which code owner approvals are required. To make
    callers aware of this the returned file code owner statuses contain code
    owner statuses for the old and new path, if they are present.
    
    A path in a change can have the following code owner statuses:
    
    * INSUFFICIENT_REVIEWERS:
      The path needs a code owner approval, but none of its code owners is
      currently a reviewer of the change.
    
    * PENDING:
      A code owner of this path has been added as reviewer, but no code
      owner approval for this path has been given yet.
    
    * APPROVED:
      The path has been approved by a code owner or a code owners override
      is present.
    
    The GetCodeOwnerStatuses REST endpoint is offered on change level,
    rather than on revision level because:
    
    * The code owner statuses are based on votes and we always show the
      current votes on the change screen even if users view an old revision
      / patch set (it would be confusing for users to see the current votes,
      but non-matching code owner statuses).
    
    * The code owner statuses for old revisions are never relevant and may
      confuse users (e.g. the code owner status 'PENDING' means that we are
      waiting for the code owner approval of a current reviewer, but the UI
      doesn’t allow reviewers to vote on non-current revisions, so that this
      approval can never happen).
    
    This means the frontend can only show owner statuses for the current
    patch set.
    
    In addition there is a risk for races if new patch sets are uploaded:
    
    1. The frontend loads the change when patch set N is current.
    2. A user uploads a new patch set N+1.
    3. The frontend loads the owner statuses which are returned for patch
       set N+1.
    
    To allow the frontend to detect this situation, the response includes
    the patch set for which the owner statuses were returned (see field
    patch_set_number). The frontend can then show a warning or do a refresh
    when the patch sets mismatch.
    
    To avoid this issue we considered to return the owner statuses as part
    of ChangeInfo (plugins field), but we know from the checks plugin that
    this is not working well:
    
    * the code owner statuses should not be returned by default, but only
      when requested (most callers don’t need the code owner statuses)
    
    * this means the caller would need to set an option on the request to
      include the code owner statuses
    
    * the frontend loads the ChangeInfo before it knows which plugins are
      active, hence at the moment when the ChangeInfo is loaded the frontend
      doesn’t know yet that it needs the code owner statuses
    
    We also considered returning the file code owner statuses as map of file
    path to file code owner status, but this doesn't work well when a file
    is renamed, as in this case the files has 2 code owner statuses, one for
    the old path and one for the new path. We could have returned both path
    in the map but then the client would have needed to figure out which
    2 paths form a file rename.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I6c449448e9d541a6d36ea9ac4805248a6cd6ed60

---
+++ javatests/com/google/gerrit/plugins/codeowners/restapi/CodeOwnerStatusInfoJsonTest.java
@@ -0,0 +1,268 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.restapi;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.plugins.codeowners.testing.CodeOwnerStatusInfoSubject.assertThat;
+import static com.google.gerrit.plugins.codeowners.testing.FileCodeOwnerStatusInfoSubject.assertThat;
+import static com.google.gerrit.plugins.codeowners.testing.PathCodeOwnerStatusInfoSubject.assertThat;
+import static com.google.gerrit.testing.GerritJUnit.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.gerrit.entities.Change;
+import com.google.gerrit.entities.PatchSet;
+import com.google.gerrit.extensions.common.ChangeType;
+import com.google.gerrit.plugins.codeowners.api.CodeOwnerStatus;
+import com.google.gerrit.plugins.codeowners.api.CodeOwnerStatusInfo;
+import com.google.gerrit.plugins.codeowners.api.FileCodeOwnerStatusInfo;
+import com.google.gerrit.plugins.codeowners.api.PathCodeOwnerStatusInfo;
+import com.google.gerrit.plugins.codeowners.backend.ChangedFile;
+import com.google.gerrit.plugins.codeowners.backend.FileCodeOwnerStatus;
+import com.google.gerrit.plugins.codeowners.backend.PathCodeOwnerStatus;
+import com.google.gerrit.plugins.codeowners.testing.FileCodeOwnerStatusInfoSubject;
+import com.google.gerrit.truth.ListSubject;
+import java.nio.file.Paths;
+import java.util.Optional;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.junit.Test;
+
+/** Tests for {@link CodeOwnerStatusInfoJson}. */
+public class CodeOwnerStatusInfoJsonTest {
+  @Test
+  public void cannotFormatNullPathCodeOwnerStatus() throws Exception {
+    NullPointerException npe =
+        assertThrows(
+            NullPointerException.class,
+            () -> CodeOwnerStatusInfoJson.format((PathCodeOwnerStatus) null));
+    assertThat(npe).hasMessageThat().isEqualTo("pathCodeOwnerStatus");
+  }
+
+  @Test
+  public void formatPathCodeOwnerStatus() throws Exception {
+    PathCodeOwnerStatus pathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/bar.baz"), CodeOwnerStatus.APPROVED);
+    PathCodeOwnerStatusInfo pathCodeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(pathCodeOwnerStatus);
+    assertThat(pathCodeOwnerStatusInfo).hasPathThat().isEqualTo("foo/bar.baz");
+    assertThat(pathCodeOwnerStatusInfo).hasStatusThat().isEqualTo(CodeOwnerStatus.APPROVED);
+  }
+
+  @Test
+  public void cannotFormatNullFileCodeOwnerStatus() throws Exception {
+    NullPointerException npe =
+        assertThrows(
+            NullPointerException.class,
+            () -> CodeOwnerStatusInfoJson.format((FileCodeOwnerStatus) null));
+    assertThat(npe).hasMessageThat().isEqualTo("fileCodeOwnerStatus");
+  }
+
+  @Test
+  public void formatFileCodeOwnerStatusForNewFile() throws Exception {
+    ChangedFile changedFile = mock(ChangedFile.class);
+    when(changedFile.changeType()).thenReturn(DiffEntry.ChangeType.ADD);
+    PathCodeOwnerStatus pathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/bar.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus =
+        FileCodeOwnerStatus.create(changedFile, Optional.of(pathCodeOwnerStatus), Optional.empty());
+    FileCodeOwnerStatusInfo fileCodeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(fileCodeOwnerStatus);
+    assertThat(fileCodeOwnerStatusInfo).hasChangeTypeThat().isEqualTo(ChangeType.ADDED);
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasNewPathStatusThat()
+        .value()
+        .hasPathThat()
+        .isEqualTo("foo/bar.baz");
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasNewPathStatusThat()
+        .value()
+        .hasStatusThat()
+        .isEqualTo(CodeOwnerStatus.APPROVED);
+    assertThat(fileCodeOwnerStatusInfo).hasOldPathStatusThat().isEmpty();
+  }
+
+  @Test
+  public void formatFileCodeOwnerStatusForModifiedFile() throws Exception {
+    ChangedFile changedFile = mock(ChangedFile.class);
+    when(changedFile.changeType()).thenReturn(DiffEntry.ChangeType.MODIFY);
+    PathCodeOwnerStatus pathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/bar.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus =
+        FileCodeOwnerStatus.create(changedFile, Optional.of(pathCodeOwnerStatus), Optional.empty());
+    FileCodeOwnerStatusInfo fileCodeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(fileCodeOwnerStatus);
+    assertThat(fileCodeOwnerStatusInfo).hasChangeTypeThat().isNull();
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasNewPathStatusThat()
+        .value()
+        .hasPathThat()
+        .isEqualTo("foo/bar.baz");
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasNewPathStatusThat()
+        .value()
+        .hasStatusThat()
+        .isEqualTo(CodeOwnerStatus.APPROVED);
+    assertThat(fileCodeOwnerStatusInfo).hasOldPathStatusThat().isEmpty();
+  }
+
+  @Test
+  public void formatFileCodeOwnerStatusForDeletedFile() throws Exception {
+    ChangedFile changedFile = mock(ChangedFile.class);
+    when(changedFile.changeType()).thenReturn(DiffEntry.ChangeType.DELETE);
+    PathCodeOwnerStatus pathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/bar.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus =
+        FileCodeOwnerStatus.create(changedFile, Optional.empty(), Optional.of(pathCodeOwnerStatus));
+    FileCodeOwnerStatusInfo fileCodeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(fileCodeOwnerStatus);
+    assertThat(fileCodeOwnerStatusInfo).hasChangeTypeThat().isEqualTo(ChangeType.DELETED);
+    assertThat(fileCodeOwnerStatusInfo).hasNewPathStatusThat().isEmpty();
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasOldPathStatusThat()
+        .value()
+        .hasPathThat()
+        .isEqualTo("foo/bar.baz");
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasOldPathStatusThat()
+        .value()
+        .hasStatusThat()
+        .isEqualTo(CodeOwnerStatus.APPROVED);
+  }
+
+  @Test
+  public void formatFileCodeOwnerStatusForRenamedFile() throws Exception {
+    ChangedFile changedFile = mock(ChangedFile.class);
+    when(changedFile.changeType()).thenReturn(DiffEntry.ChangeType.RENAME);
+    PathCodeOwnerStatus newPathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/new.baz"), CodeOwnerStatus.PENDING);
+    PathCodeOwnerStatus oldPathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/old.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus =
+        FileCodeOwnerStatus.create(
+            changedFile, Optional.of(newPathCodeOwnerStatus), Optional.of(oldPathCodeOwnerStatus));
+    FileCodeOwnerStatusInfo fileCodeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(fileCodeOwnerStatus);
+    assertThat(fileCodeOwnerStatusInfo).hasChangeTypeThat().isEqualTo(ChangeType.RENAMED);
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasNewPathStatusThat()
+        .value()
+        .hasPathThat()
+        .isEqualTo("foo/new.baz");
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasNewPathStatusThat()
+        .value()
+        .hasStatusThat()
+        .isEqualTo(CodeOwnerStatus.PENDING);
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasOldPathStatusThat()
+        .value()
+        .hasPathThat()
+        .isEqualTo("foo/old.baz");
+    assertThat(fileCodeOwnerStatusInfo)
+        .hasOldPathStatusThat()
+        .value()
+        .hasStatusThat()
+        .isEqualTo(CodeOwnerStatus.APPROVED);
+  }
+
+  @Test
+  public void cannotFormatCodeOwnerStatusInfoForNullFileCodeOwnerStatuses() throws Exception {
+    NullPointerException npe =
+        assertThrows(
+            NullPointerException.class,
+            () -> CodeOwnerStatusInfoJson.format(PatchSet.id(Change.id(1), 1), null));
+    assertThat(npe).hasMessageThat().isEqualTo("fileCodeOwnerStatuses");
+  }
+
+  @Test
+  public void cannotCodeOwnerStatusInfoForNullPathSetId() throws Exception {
+    NullPointerException npe =
+        assertThrows(
+            NullPointerException.class,
+            () -> CodeOwnerStatusInfoJson.format(null, ImmutableSet.of()));
+    assertThat(npe).hasMessageThat().isEqualTo("patchSetId");
+  }
+
+  @Test
+  public void formatCodeOwnerStatusInfo() throws Exception {
+    ChangedFile changedFile = mock(ChangedFile.class);
+    when(changedFile.changeType()).thenReturn(DiffEntry.ChangeType.ADD);
+    PathCodeOwnerStatus pathCodeOwnerStatus =
+        PathCodeOwnerStatus.create(Paths.get("/foo/bar.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus =
+        FileCodeOwnerStatus.create(changedFile, Optional.of(pathCodeOwnerStatus), Optional.empty());
+    CodeOwnerStatusInfo codeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(
+            PatchSet.id(Change.id(1), 1), ImmutableSet.of(fileCodeOwnerStatus));
+    assertThat(codeOwnerStatusInfo).hasPatchSetNumberThat().isEqualTo(1);
+    FileCodeOwnerStatusInfoSubject fileCodeOwnerStatusInfoSubject =
+        assertThat(codeOwnerStatusInfo).hasFileCodeOwnerStatusesThat().onlyElement();
+    fileCodeOwnerStatusInfoSubject.hasChangeTypeThat().isEqualTo(ChangeType.ADDED);
+    fileCodeOwnerStatusInfoSubject
+        .hasNewPathStatusThat()
+        .value()
+        .hasPathThat()
+        .isEqualTo("foo/bar.baz");
+    fileCodeOwnerStatusInfoSubject
+        .hasNewPathStatusThat()
+        .value()
+        .hasStatusThat()
+        .isEqualTo(CodeOwnerStatus.APPROVED);
+    fileCodeOwnerStatusInfoSubject.hasOldPathStatusThat().isEmpty();
+  }
+
+  @Test
+  public void fileCodeOwnerStatusInfosInCodeOwnerStatusInfoAreSortedByPath() throws Exception {
+    ChangedFile changedFile1 = mock(ChangedFile.class);
+    when(changedFile1.changeType()).thenReturn(DiffEntry.ChangeType.ADD);
+    PathCodeOwnerStatus pathCodeOwnerStatus1 =
+        PathCodeOwnerStatus.create(Paths.get("/foo/a/bar.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus1 =
+        FileCodeOwnerStatus.create(
+            changedFile1, Optional.of(pathCodeOwnerStatus1), Optional.empty());
+
+    ChangedFile changedFile2 = mock(ChangedFile.class);
+    when(changedFile2.changeType()).thenReturn(DiffEntry.ChangeType.DELETE);
+    PathCodeOwnerStatus pathCodeOwnerStatus2 =
+        PathCodeOwnerStatus.create(Paths.get("/foo/b/bar.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus2 =
+        FileCodeOwnerStatus.create(
+            changedFile2, Optional.empty(), Optional.of(pathCodeOwnerStatus2));
+
+    ChangedFile changedFile3 = mock(ChangedFile.class);
+    when(changedFile3.changeType()).thenReturn(DiffEntry.ChangeType.DELETE);
+    PathCodeOwnerStatus newPathCodeOwnerStatus3 =
+        PathCodeOwnerStatus.create(Paths.get("/foo/c/new.baz"), CodeOwnerStatus.APPROVED);
+    PathCodeOwnerStatus oldPathCodeOwnerStatus3 =
+        PathCodeOwnerStatus.create(Paths.get("/foo/c/old.baz"), CodeOwnerStatus.APPROVED);
+    FileCodeOwnerStatus fileCodeOwnerStatus3 =
+        FileCodeOwnerStatus.create(
+            changedFile3,
+            Optional.of(newPathCodeOwnerStatus3),
+            Optional.of(oldPathCodeOwnerStatus3));
+
+    CodeOwnerStatusInfo codeOwnerStatusInfo =
+        CodeOwnerStatusInfoJson.format(
+            PatchSet.id(Change.id(1), 1),
+            ImmutableSet.of(fileCodeOwnerStatus3, fileCodeOwnerStatus2, fileCodeOwnerStatus1));
+    ListSubject<FileCodeOwnerStatusInfoSubject, FileCodeOwnerStatusInfo> listSubject =
+        assertThat(codeOwnerStatusInfo).hasFileCodeOwnerStatusesThat();
+    listSubject.hasSize(3);
+    listSubject.element(0).hasNewPathStatusThat().value().hasPathThat().isEqualTo("foo/a/bar.baz");
+    listSubject.element(1).hasOldPathStatusThat().value().hasPathThat().isEqualTo("foo/b/bar.baz");
+    listSubject.element(2).hasNewPathStatusThat().value().hasPathThat().isEqualTo("foo/c/new.baz");
+  }
+}
