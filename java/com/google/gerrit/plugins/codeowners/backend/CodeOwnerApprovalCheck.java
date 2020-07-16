09b069bef38af8b881945b6684f0d204bb785935
Edwin Kempin <ekempin@google.com>
2020-07-16 09:12:05 +0200

    Assume implicit approval from patch set uploader for owned files
    If the uploader of the current patch set owns any of the touched files,
    these files are now automatically considered as approved by the patch
    set uploader.
    
    This uses the uploader of the current patch set, rather than the
    committer or author because the uploader cannot easily be forged.
    Committer and author can be forged easily if the user has the
    corresponding permissions and also forging them is normal for some use
    cases (e.g. the author is forged if a user uploads a patch set to a
    change of another user, for example while the change author is in
    vacation). If we would assume implicit approvals from the committer or
    author this would be unsafe, since this way anyone who can forge
    committer or author could approve file modifications in their name by
    forging their identity. This is also the reason why the
    ignoreSelfApproval rule on label types [1] prevents self approval from
    the uploader rather than from the committer or author.
    
    [1] https://gerrit-review.googlesource.com/Documentation/config-labels.html#label_ignoreSelfApproval
    
    Change-Id: I58efd122748f14b385d21a447e93fc27c076d5c6
    Signed-off-by: Edwin Kempin <ekempin@google.com>

--- java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerApprovalCheck.java
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerApprovalCheck.java
@@ -86,6 +86,7 @@ public class CodeOwnerApprovalCheck {
 
     ChangeNotes changeNotes = revisionResource.getNotes();
     BranchNameKey branch = changeNotes.getChange().getDest();
+    Account.Id patchSetUploader = revisionResource.getPatchSet().uploader();
 
     ImmutableSet<Account.Id> reviewerAccountIds = getReviewerAccountIds(changeNotes);
     ImmutableSet<Account.Id> approverAccountIds =
@@ -102,7 +103,11 @@ public class CodeOwnerApprovalCheck {
         newPathStatus =
             Optional.of(
                 getCodeOwnerStatus(
-                    branch, reviewerAccountIds, approverAccountIds, changedFile.newPath().get()));
+                    branch,
+                    patchSetUploader,
+                    reviewerAccountIds,
+                    approverAccountIds,
+                    changedFile.newPath().get()));
       }
 
       // Compute the code owner status for the old path, if the file was deleted or renamed.
@@ -113,7 +118,11 @@ public class CodeOwnerApprovalCheck {
         oldPathStatus =
             Optional.of(
                 getCodeOwnerStatus(
-                    branch, reviewerAccountIds, approverAccountIds, changedFile.oldPath().get()));
+                    branch,
+                    patchSetUploader,
+                    reviewerAccountIds,
+                    approverAccountIds,
+                    changedFile.oldPath().get()));
       }
 
       fileCodeOwnerStatusBuilder.add(
@@ -125,18 +134,11 @@ public class CodeOwnerApprovalCheck {
 
   private PathCodeOwnerStatus getCodeOwnerStatus(
       BranchNameKey branch,
+      Account.Id patchSetUploader,
       ImmutableSet<Account.Id> reviewerAccountIds,
       ImmutableSet<Account.Id> approverAccountIds,
       Path absolutePath)
       throws InvalidPluginConfigurationException {
-    // TODO(ekempin): Check if the path is owned by the uploader of the patch set since paths that
-    // are owned by the patch set uploader should be considered as approved.
-    if (reviewerAccountIds.isEmpty()) {
-      // Short-cut, if there are no reviewers, any path has the INSUFFICIENT_REVIEWERS code owner
-      // status.
-      return PathCodeOwnerStatus.create(absolutePath, CodeOwnerStatus.INSUFFICIENT_REVIEWERS);
-    }
-
     AtomicReference<CodeOwnerStatus> codeOwnerStatus =
         new AtomicReference<>(CodeOwnerStatus.INSUFFICIENT_REVIEWERS);
     codeOwnerConfigHierarchy.visit(
@@ -145,6 +147,15 @@ public class CodeOwnerApprovalCheck {
         codeOwnerConfig -> {
           ImmutableSet<Account.Id> codeOwnerAccountIds =
               getCodeOwnerAccountIds(codeOwnerConfig, absolutePath);
+
+          if (codeOwnerAccountIds.contains(patchSetUploader)) {
+            // If the uploader of the patch set owns the path, there is an implicit code owner
+            // approval from the patch set uploader so that the path is automatically approved.
+            codeOwnerStatus.set(CodeOwnerStatus.APPROVED);
+
+            // We can abort since we already found that the path was approved.
+            return false;
+          }
 
           if (Collections.disjoint(codeOwnerAccountIds, reviewerAccountIds)) {
             // We need to continue to check if any of the higher-level code owners is a reviewer.
