fafec6921babd124281f484405bb66a919bdf831
Edwin Kempin <ekempin@google.com>
2020-07-30 11:13:07 +0200

    Make code to read and validate required approval config generic
    This will allow us to reuse this code for further configuration
    parameter (namely the configuration parameter to configure which
    approval is required for code owner overrides that will be added in a
    follow-up change).
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Ic737ac123540fb83688c6e278005ec0507668ac1

--- java/com/google/gerrit/plugins/codeowners/config/RequiredApprovalConfig.java
+++ java/com/google/gerrit/plugins/codeowners/config/RequiredApprovalConfig.java
@@ -14,20 +14,12 @@
 
 package com.google.gerrit.plugins.codeowners.config;
 
-import static com.google.gerrit.plugins.codeowners.config.CodeOwnersPluginConfiguration.SECTION_CODE_OWNERS;
-import static java.util.Objects.requireNonNull;
-
 import com.google.common.annotations.VisibleForTesting;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.server.config.PluginConfigFactory;
-import com.google.gerrit.server.git.validators.CommitValidationMessage;
-import com.google.gerrit.server.git.validators.ValidationMessage;
-import com.google.gerrit.server.project.ProjectLevelConfig;
 import com.google.gerrit.server.project.ProjectState;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.util.Optional;
-import org.eclipse.jgit.lib.Config;
 
 /**
  * Class to read the required approval configuration from {@code gerrit.config} and from {@code
@@ -43,7 +35,7 @@ import org.eclipse.jgit.lib.Config;
  * parent projects.
  */
 @Singleton
-public class RequiredApprovalConfig {
+public class RequiredApprovalConfig extends AbstractRequiredApprovalConfig {
   @VisibleForTesting public static final String KEY_REQUIRED_APPROVAL = "requiredApproval";
 
   /** By default a {@code Code-Review+1} vote from a code owner approves the file. */
@@ -51,94 +43,14 @@ public class RequiredApprovalConfig {
 
   @VisibleForTesting public static final short DEFAULT_VALUE = 1;
 
-  private final String pluginName;
-  private final PluginConfigFactory pluginConfigFactory;
-
   @Inject
   RequiredApprovalConfig(@PluginName String pluginName, PluginConfigFactory pluginConfigFactory) {
-    this.pluginName = pluginName;
-    this.pluginConfigFactory = pluginConfigFactory;
+    super(pluginName, pluginConfigFactory);
   }
 
-  Optional<RequiredApproval> getForProject(ProjectState projectState, Config pluginConfig) {
-    requireNonNull(projectState, "projectState");
-    requireNonNull(pluginConfig, "pluginConfig");
-    String requiredApproval =
-        pluginConfig.getString(SECTION_CODE_OWNERS, null, KEY_REQUIRED_APPROVAL);
-    if (requiredApproval == null) {
-      return Optional.empty();
-    }
-
-    try {
-      return Optional.of(RequiredApproval.parse(projectState, requiredApproval));
-    } catch (IllegalStateException | IllegalArgumentException e) {
-      throw new InvalidPluginConfigurationException(
-          pluginName,
-          String.format(
-              "Required approval '%s' that is configured in %s.config"
-                  + " (parameter %s.%s) is invalid: %s",
-              requiredApproval,
-              pluginName,
-              SECTION_CODE_OWNERS,
-              KEY_REQUIRED_APPROVAL,
-              e.getMessage()));
-    }
-  }
-
-  Optional<RequiredApproval> getFromGlobalPluginConfig(ProjectState projectState) {
-    requireNonNull(projectState, "projectState");
-
-    String requiredApproval =
-        pluginConfigFactory.getFromGerritConfig(pluginName).getString(KEY_REQUIRED_APPROVAL);
-    if (requiredApproval == null) {
-      return Optional.empty();
-    }
-
-    try {
-      return Optional.of(RequiredApproval.parse(projectState, requiredApproval));
-    } catch (IllegalStateException | IllegalArgumentException e) {
-      throw new InvalidPluginConfigurationException(
-          pluginName,
-          String.format(
-              "Required approval '%s' that is configured in gerrit.config"
-                  + " (parameter plugin.%s.%s) is invalid: %s",
-              requiredApproval, pluginName, KEY_REQUIRED_APPROVAL, e.getMessage()));
-    }
-  }
-
-  /**
-   * Validates the required approval configuration in the given project level configuration.
-   *
-   * @param fileName the name of the config file
-   * @param projectLevelConfig the project level plugin configuration
-   * @return list of validation messages for validation errors, empty list if there are no
-   *     validation errors
-   */
-  Optional<CommitValidationMessage> validateProjectLevelConfig(
-      ProjectState projectState, String fileName, ProjectLevelConfig projectLevelConfig) {
-    requireNonNull(projectState, "projectState");
-    requireNonNull(fileName, "fileName");
-    requireNonNull(projectLevelConfig, "projectLevelConfig");
-
-    String requiredApproval =
-        projectLevelConfig.get().getString(SECTION_CODE_OWNERS, null, KEY_REQUIRED_APPROVAL);
-    if (requiredApproval != null) {
-      try {
-        RequiredApproval.parse(projectState, requiredApproval);
-      } catch (IllegalArgumentException | IllegalStateException e) {
-        return Optional.of(
-            new CommitValidationMessage(
-                String.format(
-                    "Required approval '%s' that is configured in %s (parameter %s.%s) is invalid: %s",
-                    requiredApproval,
-                    fileName,
-                    SECTION_CODE_OWNERS,
-                    KEY_REQUIRED_APPROVAL,
-                    e.getMessage()),
-                ValidationMessage.Type.ERROR));
-      }
-    }
-    return Optional.empty();
+  @Override
+  protected String getConfigKey() {
+    return KEY_REQUIRED_APPROVAL;
   }
 
   RequiredApproval createDefault(ProjectState projectState) throws IllegalStateException {
