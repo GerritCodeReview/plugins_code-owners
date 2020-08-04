de3c357d5b5426b8efc893f18abd0a17678154da
Edwin Kempin <ekempin@google.com>
2020-08-04 11:55:44 +0200

    Add global imports to code owner configs
    This change adds a field for global imports to the CodeOwnerConfig
    class. Populating and reading this field will be implemented in
    follow-up changes, as well as support for per-file imports (imports that
    apply only for given path expressions).
    
    Code owner configs can import other code owner configs from:
    1. a different path in the same project
    2. any path in another project (same host)
    
    Being able to specify the branch from which the code owner config is
    imported is desired too, but will be implemented only later (we first
    want to cover the import functionality that is currently supported by
    the find-owners plugin and the find-owners plugin doesn't support
    specifying branches for imports).
    
    Paths of imported code owner configs can be relative or absolute.
    
    The find-owners plugin currently supports 2 import modes:
    1. using the "file" directive (e.g. "file: <file>"):
       only the global code owners from the imported code owner config are
       considered (code owners from code owner sets without path
       expressions)
    2. using the "include" directive (e.g. "include <file>"):
       imports global and per-file code owners (code owners from all code
       owner sets) and also the "set noparent" flag
    
    Details about these import modes are described at [2].
    
    Since we need to be compatible with the find-owners plugin we need to
    support these import modes too. In the code we represent them via the
    CodeOwnerConfigImportMode enum. The CodeOwnerConfigImportMode exposes a
    bunch of methods to control the import behaviour. This way we can easily
    tweak the behaviour later.
    
    Imports are represented as CodeOwnerConfigReference's and are stored in
    CodeOwnerConfig, instead of loading and resolving them immediately on
    parse of the code owner config. This way we can resolve the imports in
    the generic layer rather than in the backend layer. This is important
    since imports from other projects are possible and these other projects
    may use different code owner backends, and the logic to lookup the code
    owner backend for a project lives in the generic layer.
    
    [1] https://gerrit.googlesource.com/plugins/find-owners/+/73bb362fd84b2089e2625761253852cc7716a14b/src/main/resources/Documentation/syntax.md
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I46005fbab6e127de66ac38d3d6de076e36cf3f3e

---
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerConfigReference.java
@@ -0,0 +1,131 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.auto.value.AutoValue;
+import com.google.gerrit.entities.Project;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+/**
+ * A reference to a {@link CodeOwnerConfig}.
+ *
+ * <p>Allows a {@link CodeOwnerConfig} to include other {@link CodeOwnerConfig}s.
+ */
+@AutoValue
+public abstract class CodeOwnerConfigReference {
+  /**
+   * Gets the import mode that controls which parts of the referenced code owner config should be
+   * imported.
+   */
+  public abstract CodeOwnerConfigImportMode importMode();
+
+  /**
+   * The project in which the code owner config is stored.
+   *
+   * <p>If not set, the project is the same as the project of the code owner config that contains
+   * this code owner config reference.
+   */
+  public abstract Optional<Project.NameKey> project();
+
+  /**
+   * The path of the code owner config file.
+   *
+   * <p>May be absolute or relative to the path of the importing code owner config.
+   */
+  public abstract Path filePath();
+
+  /**
+   * The path of the folder that contains the code owner config.
+   *
+   * <p>May be absolute or relative to the path of the importing code owner config.
+   */
+  public Path path() {
+    return firstNonNull(filePath().getParent(), Paths.get(""));
+  }
+
+  /** The name of the code owner config file. */
+  public String fileName() {
+    return filePath().getFileName().toString();
+  }
+
+  /**
+   * Creates a builder for a code owner config.
+   *
+   * @param importMode the import mode
+   * @param filePath the path of the code owner config, may be absolute or relative to the path of
+   *     the importing code owner config
+   * @return builder for a code owner config
+   */
+  public static Builder builder(CodeOwnerConfigImportMode importMode, String filePath) {
+    requireNonNull(filePath, "filePath");
+    return builder(importMode, Paths.get(filePath));
+  }
+
+  /**
+   * Creates a builder for a code owner config.
+   *
+   * @param importMode the import mode
+   * @param filePath the path of the code owner config, may be absolute or relative to the path of
+   *     the importing code owner config
+   * @return builder for a code owner config
+   */
+  public static Builder builder(CodeOwnerConfigImportMode importMode, Path filePath) {
+    return new AutoValue_CodeOwnerConfigReference.Builder()
+        .setImportMode(importMode)
+        .setFilePath(filePath);
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    /**
+     * Sets the import mode that controls which parts of the referenced code owner config should be
+     * imported.
+     *
+     * @param codeOwnerConfigImportMode the import mode that controls which parts of the referenced
+     *     code owner config should be imported
+     * @return the Builder instance for chaining calls
+     */
+    public abstract Builder setImportMode(CodeOwnerConfigImportMode codeOwnerConfigImportMode);
+
+    /**
+     * Sets the project in which the code owner config is stored.
+     *
+     * @param project the project in which the code owner config is stored
+     * @return the Builder instance for chaining calls
+     */
+    public abstract Builder setProject(Project.NameKey project);
+
+    /**
+     * Sets the path of the code owner config file.
+     *
+     * @param filePath path of the code owner config file, may be absolute or relative to the path
+     *     of the importing code owner config
+     * @return the Builder instance for chaining calls
+     */
+    abstract Builder setFilePath(Path filePath);
+
+    /**
+     * Builds the {@link CodeOwnerConfigReference} instance.
+     *
+     * @return the {@link CodeOwnerConfigReference} instance
+     */
+    public abstract CodeOwnerConfigReference build();
+  }
+}
