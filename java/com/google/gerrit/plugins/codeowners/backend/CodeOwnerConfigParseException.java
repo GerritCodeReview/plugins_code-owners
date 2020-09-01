ffc58418edb5e4420dfa796a23523de43c2f8c61
Edwin Kempin <ekempin@google.com>
2020-09-01 11:50:21 +0200

    Reject non-parseable code owner config files on push
    If there are non-parseable code owner config files in the repository
    changes that touch files for which these code owner configs are relevant
    cannot be submitted (since the code owners cannot be computed). To
    prevent this from happening we now disallow adding non-parseable code
    owner configs and reject them directly on push. Messages that describe
    the validation errors are included in the push messages so that the user
    can find out what needs to be fixed.
    
    With this change we only ensure that the code owner config files are
    parseable. In follow-up changes we will add further validation that will
    be done on the parsed code owner config files (see todo in
    CodeOwnerConfigValidator).
    
    If the code-owners plugin configuration is invalid we cannot get the
    code owners backend and hence we are not able to detect and validate
    code owner config files. Instead of failing in this case (which would
    block all change uploads) we only log a warning and accept that it's
    possible to add invalid code owner configs while the plugin
    configuration is invalid.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I96148b6a5797551ee60e4f96be45a05bded25c31

---
+++ java/com/google/gerrit/plugins/codeowners/backend/CodeOwnerConfigParseException.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.server.git.ValidationError;
+import com.google.gerrit.server.validators.ValidationException;
+import java.util.List;
+
+/** Exception that is thrown if a code owner config cannot be parsed. */
+public class CodeOwnerConfigParseException extends ValidationException {
+  private static final long serialVersionUID = 1L;
+
+  private final CodeOwnerConfig.Key codeOwnerConfigKey;
+  private final ImmutableList<ValidationError> messages;
+
+  public CodeOwnerConfigParseException(
+      CodeOwnerConfig.Key codeOwnerConfigKey, List<ValidationError> messages) {
+    super("invalid code owner config file");
+    this.codeOwnerConfigKey = codeOwnerConfigKey;
+    this.messages = ImmutableList.copyOf(messages);
+  }
+
+  public CodeOwnerConfigParseException(CodeOwnerConfig.Key codeOwnerConfigKey, String message) {
+    this(codeOwnerConfigKey, ImmutableList.of(ValidationError.create(message)));
+  }
+
+  /** Returns all validation as a single, formatted string. */
+  public String getFullMessage(String defaultCodeOwnerConfigFileName) {
+    StringBuilder sb = new StringBuilder(getMessage());
+    sb.append(" '").append(codeOwnerConfigKey.filePath(defaultCodeOwnerConfigFileName)).append("'");
+    if (!messages.isEmpty()) {
+      sb.append(':');
+      for (ValidationError msg : messages) {
+        sb.append("\n  ").append(msg.getMessage());
+      }
+    }
+    return sb.toString();
+  }
+}
---
+++ java/com/google/gerrit/plugins/codeowners/validation/CodeOwnerConfigValidator.java
@@ -0,0 +1,174 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.validation;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.BranchNameKey;
+import com.google.gerrit.exceptions.StorageException;
+import com.google.gerrit.plugins.codeowners.JgitPath;
+import com.google.gerrit.plugins.codeowners.backend.ChangedFiles;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerBackend;
+import com.google.gerrit.plugins.codeowners.backend.CodeOwnerConfig;
+import com.google.gerrit.plugins.codeowners.config.CodeOwnersPluginConfiguration;
+import com.google.gerrit.plugins.codeowners.config.InvalidPluginConfigurationException;
+import com.google.gerrit.server.events.CommitReceivedEvent;
+import com.google.gerrit.server.git.validators.CommitValidationException;
+import com.google.gerrit.server.git.validators.CommitValidationListener;
+import com.google.gerrit.server.git.validators.CommitValidationMessage;
+import com.google.gerrit.server.git.validators.ValidationMessage;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.ObjectId;
+
+/** Validates modifications to the code owner config files. */
+@Singleton
+public class CodeOwnerConfigValidator implements CommitValidationListener {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final CodeOwnersPluginConfiguration codeOwnersPluginConfiguration;
+  private final ChangedFiles changedFiles;
+
+  @Inject
+  CodeOwnerConfigValidator(
+      CodeOwnersPluginConfiguration codeOwnersPluginConfiguration, ChangedFiles changedFiles) {
+    this.codeOwnersPluginConfiguration = codeOwnersPluginConfiguration;
+    this.changedFiles = changedFiles;
+  }
+
+  @Override
+  public List<CommitValidationMessage> onCommitReceived(CommitReceivedEvent receiveEvent)
+      throws CommitValidationException {
+    if (codeOwnersPluginConfiguration.isDisabled(receiveEvent.getBranchNameKey())) {
+      return ImmutableList.of();
+    }
+
+    try {
+      CodeOwnerBackend codeOwnerBackend =
+          codeOwnersPluginConfiguration.getBackend(receiveEvent.getBranchNameKey());
+
+      ImmutableList<CommitValidationMessage> validationMessages =
+          // iterate over all changed files
+          changedFiles.compute(receiveEvent.getProjectNameKey(), receiveEvent.commit).stream()
+              // filter out deletions (files without new path)
+              .filter(changedFile -> changedFile.newPath().isPresent())
+              // map to path
+              .map(changedFile -> changedFile.newPath().get().toString())
+              // filter out non code owner config files
+              .filter(
+                  filePath ->
+                      codeOwnerBackend.isCodeOwnerConfigFile(
+                          receiveEvent.getProjectNameKey(),
+                          Paths.get(filePath).getFileName().toString()))
+              // validate the code owner config files
+              .flatMap(
+                  filePath ->
+                      validateCodeOwnerConfig(
+                          codeOwnerBackend,
+                          receiveEvent.getBranchNameKey(),
+                          filePath,
+                          receiveEvent.commit))
+              .collect(toImmutableList());
+
+      if (validationMessages.stream()
+          .anyMatch(
+              validationMessage ->
+                  ValidationMessage.Type.ERROR.equals(validationMessage.getType()))) {
+        throw new CommitValidationException("invalid code owner config files", validationMessages);
+      }
+      return validationMessages;
+    } catch (InvalidPluginConfigurationException e) {
+      // If the code-owners plugin configuration is invalid we cannot get the code owners backend
+      // and hence we are not able to detect and validate code owner config files. Instead of
+      // failing in this case (which would block all change uploads) we only log a warning and
+      // accept that it's possible to add invalid code owner configs while the plugin configuration
+      // is invalid.
+      logger.atWarning().log(
+          String.format(
+              "cannot validate code owner config files due to invalid code-owners plugin configuration: %s",
+              e.getMessage()));
+      return ImmutableList.of();
+    } catch (IOException e) {
+      String errorMessage =
+          String.format(
+              "failed to validate code owner config files in revision %s (project = %s, branch = %s)",
+              receiveEvent.commit.getName(),
+              receiveEvent.getProjectNameKey(),
+              receiveEvent.getBranchNameKey().branch());
+      logger.atSevere().log(errorMessage);
+      throw new StorageException(errorMessage, e);
+    }
+  }
+
+  private Stream<CommitValidationMessage> validateCodeOwnerConfig(
+      CodeOwnerBackend codeOwnerBackend,
+      BranchNameKey branchNameKey,
+      String filePathAsString,
+      ObjectId revision) {
+    Path filePath = Paths.get(filePathAsString);
+    Path folderPath =
+        filePath.getParent() != null
+            ? JgitPath.of(filePath.getParent()).getAsAbsolutePath()
+            : Paths.get("/");
+    String fileName = filePath.getFileName().toString();
+    CodeOwnerConfig.Key codeOwnerConfigKey =
+        CodeOwnerConfig.Key.create(branchNameKey, folderPath, fileName);
+
+    try {
+      Optional<CodeOwnerConfig> codeOwnerConfig =
+          codeOwnerBackend.getCodeOwnerConfig(codeOwnerConfigKey, revision);
+      checkState(
+          codeOwnerConfig.isPresent(),
+          "code owner config %s not found in revision %s",
+          codeOwnerConfig,
+          revision.name());
+      return validateCodeOwnerConfig(codeOwnerConfig.get());
+    } catch (StorageException e) {
+      Optional<ConfigInvalidException> configInvalidException = getInvalidConfigCause(e);
+      if (!configInvalidException.isPresent()) {
+        throw e;
+      }
+      return Stream.of(
+          new CommitValidationMessage(
+              configInvalidException.get().getMessage(), ValidationMessage.Type.ERROR));
+    }
+  }
+
+  private Optional<ConfigInvalidException> getInvalidConfigCause(Exception e) {
+    // Check whether the exception was caused by a non-parseable code owner config (in this case
+    // the causal chain contains a ConfigInvalidException).
+    return Throwables.getCausalChain(e).stream()
+        .filter(t -> t instanceof ConfigInvalidException)
+        .map(t -> (ConfigInvalidException) t)
+        .findFirst();
+  }
+
+  private Stream<CommitValidationMessage> validateCodeOwnerConfig(
+      @SuppressWarnings("unused") CodeOwnerConfig codeOwnerConfig) {
+    // TODO(ekempin): Validate the parsed code owner config.
+    return Stream.of();
+  }
+}
