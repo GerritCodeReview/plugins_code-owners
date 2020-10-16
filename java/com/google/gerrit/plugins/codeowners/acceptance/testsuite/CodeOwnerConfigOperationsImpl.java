79498da2ccd4fb7f40e903972946b66966c29d2d
Edwin Kempin <ekempin@google.com>
2020-10-16 12:02:27 +0200

    Add method to test API to retrieve raw file content of code owner config
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: Id885bde0f9f74ac2dd7b7b5ea672e232aee83ba9

--- java/com/google/gerrit/plugins/codeowners/acceptance/testsuite/CodeOwnerConfigOperationsImpl.java
+++ java/com/google/gerrit/plugins/codeowners/acceptance/testsuite/CodeOwnerConfigOperationsImpl.java
@@ -25,11 +25,20 @@ import com.google.gerrit.plugins.codeowners.backend.CodeOwners;
 import com.google.gerrit.plugins.codeowners.backend.CodeOwnersUpdate;
 import com.google.gerrit.plugins.codeowners.config.BackendConfig;
 import com.google.gerrit.server.ServerInitiated;
+import com.google.gerrit.server.git.GitRepositoryManager;
 import com.google.gerrit.server.project.ProjectCache;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import java.nio.file.Path;
 import java.util.Optional;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.junit.TestRepository;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevObject;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.util.RawParseUtils;
 
 /**
  * The implementation of {@link CodeOwnerConfigOperations}.
@@ -38,6 +47,7 @@ import java.util.Optional;
  * the separation between interface and implementation to enhance clarity.
  */
 public class CodeOwnerConfigOperationsImpl implements CodeOwnerConfigOperations {
+  private final GitRepositoryManager repoManager;
   private final CodeOwners codeOwners;
   private final Provider<CodeOwnersUpdate> codeOwnersUpdate;
   private final ProjectCache projectCache;
@@ -45,10 +55,12 @@ public class CodeOwnerConfigOperationsImpl implements CodeOwnerConfigOperations 
 
   @Inject
   CodeOwnerConfigOperationsImpl(
+      GitRepositoryManager repoManager,
       CodeOwners codeOwners,
       @ServerInitiated Provider<CodeOwnersUpdate> codeOwnersUpdate,
       ProjectCache projectCache,
       BackendConfig backendConfig) {
+    this.repoManager = repoManager;
     this.codeOwners = codeOwners;
     this.codeOwnersUpdate = codeOwnersUpdate;
     this.projectCache = projectCache;
@@ -142,6 +154,33 @@ public class CodeOwnerConfigOperationsImpl implements CodeOwnerConfigOperations 
       // if the project doesn't exist, use the backend which is configured on host level to compute
       // the file path
       return backendConfig.getDefaultBackend().getFilePath(codeOwnerConfigKey);
+    }
+
+    @Override
+    public String getContent() throws Exception {
+      try (TestRepository<Repository> testRepo =
+          new TestRepository<>(repoManager.openRepository(codeOwnerConfigKey.project()))) {
+        Ref ref = testRepo.getRepository().exactRef(codeOwnerConfigKey.ref());
+        if (ref == null) {
+          throw new IllegalStateException(
+              String.format("code owner config %s does not exist", codeOwnerConfigKey));
+        }
+        RevCommit commit = testRepo.getRevWalk().parseCommit(ref.getObjectId());
+        try (TreeWalk tw =
+            TreeWalk.forPath(
+                testRepo.getRevWalk().getObjectReader(), getJGitFilePath(), commit.getTree())) {
+          if (tw == null) {
+            throw new IllegalStateException(
+                String.format("code owner config %s does not exist", codeOwnerConfigKey));
+          }
+        }
+        RevObject blob = testRepo.get(commit.getTree(), getJGitFilePath());
+        byte[] data = testRepo.getRepository().open(blob).getCachedBytes(Integer.MAX_VALUE);
+        return RawParseUtils.decode(data);
+      } catch (RepositoryNotFoundException e) {
+        throw new IllegalStateException(
+            String.format("code owner config %s does not exist", codeOwnerConfigKey));
+      }
     }
 
     @Override
