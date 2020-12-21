ed682c3a5701558f0251d411093bcd6a508ed35a
Edwin Kempin <ekempin@google.com>
2020-12-21 14:53:01 +0100

    Test gettig code owners with invalid limit
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I8e1ad1ba8c58c1a0e432d710094c2c033c72b904

--- javatests/com/google/gerrit/plugins/codeowners/acceptance/restapi/AbstractGetCodeOwnersForPathRestIT.java
+++ javatests/com/google/gerrit/plugins/codeowners/acceptance/restapi/AbstractGetCodeOwnersForPathRestIT.java
@@ -167,6 +167,13 @@ public abstract class AbstractGetCodeOwnersForPathRestIT extends AbstractCodeOwn
   }
 
   @Test
+  public void cannotGetCodeOwnersWithInvalidLimit() throws Exception {
+    RestResponse r = adminRestSession.get(getUrl(TEST_PATH, "limit=invalid"));
+    r.assertBadRequest();
+    assertThat(r.getEntityContent()).contains("\"invalid\" is not a valid value for \"--limit\"");
+  }
+
+  @Test
   public void cannotGetCodeOwnersWithInvalidSeed() throws Exception {
     RestResponse r = adminRestSession.get(getUrl(TEST_PATH, "seed=invalid"));
     r.assertBadRequest();
