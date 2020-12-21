d164039ea2842db40c9c86e729cf9b3125644d11
Edwin Kempin <ekempin@google.com>
2020-12-21 12:33:45 +0100

    FindOwnersBackend: Fix incompatibility of handling globs
    The find-owners backend promises to be compatible with the syntax of the
    find-owners plugin, but currently the handling of '*' in globs differs.
    
    The find-owners backend in the code-owners plugin currently handles '*'s
    in globs like this:
    
    * '*': matches any string that does not include slashes
    * '**': matches any string, including slashes
    
    This matches with how globs work in Java [1], however it is incompatible
    with the find-owners plugin that handles '*'s in globs like this:
    
    * '*'/'**': matches any string, including slashes
    
    Change the find-owners backend in the code-owners plugin to be
    compatible with the find-owners plugin. For this we simply replace any
    single '*' character that appears in globs with '**' before we check if
    the glob matches.
    
    [1] https://mincong.io/2019/04/16/glob-expression-understanding/
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I1ddc8bf326bbcb8867ffd09eebc7a6739ad7d827

---
+++ java/com/google/gerrit/plugins/codeowners/backend/FindOwnersGlobMatcher.java
@@ -0,0 +1,80 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins.codeowners.backend;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.flogger.FluentLogger;
+import java.nio.file.Path;
+
+/**
+ * Glob matcher that is compatible with how globs are interpreted by the {@code find-owners} plugin.
+ *
+ * <p>This matcher has the same behaviour as the {@link GlobMatcher} exept that:
+ *
+ * <ul>
+ *   <li>'*': matches any string, including slashes (same as '**')
+ * </ul>
+ */
+public class FindOwnersGlobMatcher implements PathExpressionMatcher {
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  /** Singleton instance. */
+  public static FindOwnersGlobMatcher INSTANCE = new FindOwnersGlobMatcher();
+
+  /** Private constructor to prevent creation of further instances. */
+  private FindOwnersGlobMatcher() {}
+
+  @Override
+  public boolean matches(String glob, Path relativePath) {
+    String adaptedGlob = replaceSingleStarWithDoubleStar(glob);
+    logger.atFine().log("adapted glob = %s", adaptedGlob);
+    return GlobMatcher.INSTANCE.matches(adaptedGlob, relativePath);
+  }
+
+  /**
+   * Replaces any single '*' in the given glob with '**'. Non-single '*'s, like '**' or '***', stay
+   * unchanged.
+   *
+   * @param glob glob in which any single '*' should be replaced by '**'
+   */
+  @VisibleForTesting
+  String replaceSingleStarWithDoubleStar(String glob) {
+    StringBuilder adaptedGlob = new StringBuilder();
+    Character previousChar = null;
+    boolean maybeSingleStar = false;
+    for (char nextCharacter : glob.toCharArray()) {
+      if (maybeSingleStar && nextCharacter != '*') {
+        // the previous character was a '*' that was not preceded by '*' (maybeSingleStar == true),
+        // since the next character is not '*', we are now sure that the previous character was a
+        // single '*' which should be replaced by '**',
+        // to do this append another '*'
+        adaptedGlob.append('*');
+      }
+      adaptedGlob.append(nextCharacter);
+
+      // the current character may be a single '*' if it's not preceded by '*'
+      maybeSingleStar =
+          nextCharacter == '*' && (previousChar == null || previousChar.charValue() != '*');
+      previousChar = nextCharacter;
+    }
+
+    if (maybeSingleStar) {
+      // the last character was a '*' that was not preceded by '*'
+      adaptedGlob.append('*');
+    }
+
+    return adaptedGlob.toString();
+  }
+}
