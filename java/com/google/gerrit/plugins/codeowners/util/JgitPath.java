af66c3de2e5c0c369902b59a87e34e367ea112cd
Edwin Kempin <ekempin@google.com>
2021-01-07 15:22:12 +0100

    Move JGitPath into a seperate package
    This is a step towards splitting the monolithic plugin target across
    java packages.
    
    Also-by: David Ostrovsky <david@ostrovsky.org>
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I263f395fad4109627f9e3e20703e0c8b92fb4852

--- java/com/google/gerrit/plugins/codeowners/util/JgitPath.java
+++ java/com/google/gerrit/plugins/codeowners/util/JgitPath.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.plugins.codeowners;
+package com.google.gerrit.plugins.codeowners.util;
 
 import static java.util.Objects.requireNonNull;
 
