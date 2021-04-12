b19a239982124eb9cdf4e4646b6d0cf6605e74f2
Edwin Kempin <ekempin@google.com>
2021-04-12 14:18:35 +0200

    GetCodeOwnerStatus: Limit the number of returned statuses by default
    Computing all code owner statuses can be expensive for large changes.
    
    To avoid unintended high latency when using this REST endpoint we limit
    the results in the response by default to 200 and if callers want to get
    more results they need to set a higher limit explicitly. This behaviour
    is consistent with the get code owners REST endpoints which also have a
    default limit.
    
    By default the change screen is showing at most 200 files and if the
    user wants to see more/all files they need to expand the file list. We
    choose 200 as default limit for the code owner statuses so that these
    200 files can be covered (renames are reported as a single code owner
    status, hence also 200 renames are covered).
    
    Having this default limit means that the frontend needs to be adapted to
    request further code owner statuses when the user expands the file list
    on the change screen to all files. In this case the frontend can use the
    start parameter to skip the 200 code owner statuses that it already has.
    
    While the frontend is not adapted yet, code owner statuses are not
    available from the 201st file onwards when the user expands the file
    list.
    
    Signed-off-by: Edwin Kempin <ekempin@google.com>
    Change-Id: I73508bc23c699a4f91e70f9da041806ba7a19ce1

--- java/com/google/gerrit/plugins/codeowners/restapi/GetCodeOwnerStatus.java
+++ java/com/google/gerrit/plugins/codeowners/restapi/GetCodeOwnerStatus.java
@@ -16,6 +16,7 @@ package com.google.gerrit.plugins.codeowners.restapi;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
 import com.google.gerrit.extensions.restapi.BadRequestException;
 import com.google.gerrit.extensions.restapi.Response;
@@ -53,18 +54,19 @@ import org.kohsuke.args4j.Option;
  * </ul>
  */
 public class GetCodeOwnerStatus implements RestReadView<ChangeResource> {
-  private static final int UNLIMITED = 0;
+  @VisibleForTesting public static final int DEFAULT_LIMIT = 200;
 
   private final CodeOwnerApprovalCheck codeOwnerApprovalCheck;
 
   private int start;
-  private int limit;
+  private int limit = DEFAULT_LIMIT;
 
   @Option(
       name = "--limit",
       aliases = {"-n"},
       metaVar = "CNT",
-      usage = "maximum number of file code owner statuses to return (by default 0 aka unlimited)")
+      usage =
+          "maximum number of file code owner statuses to return (default = " + DEFAULT_LIMIT + ")")
   public void setLimit(int limit) {
     this.limit = limit;
   }
@@ -90,16 +92,12 @@ public class GetCodeOwnerStatus implements RestReadView<ChangeResource> {
     validateStartAndLimit();
 
     ImmutableSet<FileCodeOwnerStatus> fileCodeOwnerStatuses =
-        codeOwnerApprovalCheck.getFileStatusesAsSet(
-            changeResource.getNotes(), start, limit == UNLIMITED ? UNLIMITED : limit + 1);
+        codeOwnerApprovalCheck.getFileStatusesAsSet(changeResource.getNotes(), start, limit + 1);
     CodeOwnerStatusInfo codeOwnerStatusInfo =
         CodeOwnerStatusInfoJson.format(
             changeResource.getNotes().getCurrentPatchSet().id(),
-            limit == UNLIMITED
-                ? fileCodeOwnerStatuses
-                : fileCodeOwnerStatuses.stream().limit(limit).collect(toImmutableSet()));
-    codeOwnerStatusInfo.more =
-        limit != UNLIMITED && fileCodeOwnerStatuses.size() > limit ? true : null;
+            fileCodeOwnerStatuses.stream().limit(limit).collect(toImmutableSet()));
+    codeOwnerStatusInfo.more = fileCodeOwnerStatuses.size() > limit ? true : null;
     return Response.ok(codeOwnerStatusInfo);
   }
 
@@ -107,8 +105,8 @@ public class GetCodeOwnerStatus implements RestReadView<ChangeResource> {
     if (start < 0) {
       throw new BadRequestException("start cannot be negative");
     }
-    if (limit < 0) {
-      throw new BadRequestException("limit cannot be negative");
+    if (limit <= 0) {
+      throw new BadRequestException("limit must be positive");
     }
   }
 }
