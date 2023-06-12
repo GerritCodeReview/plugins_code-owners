82f55eb8598cf2cd507b739bdb4e131bcde8be0e
Dhruv Srivastava <dhruvsri@google.com>
2023-06-12 08:57:31 +0000

    Commit
    Change-Id: I37f80824576722cc8fbedabcd68203de2cf03e0b

--- java/com/google/gerrit/plugins/codeowners/api/CheckCodeOwnerConfigFilesInRevisionInput.java
+++ java/com/google/gerrit/plugins/codeowners/api/CheckCodeOwnerConfigFilesInRevisionInput.java
@@ -5,19 +5,6 @@
 // http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// limitations under the License.
-
-package com.google.gerrit.plugins.codeowners.api;
-
-  /**
-
-  /**
-   * Level that controls which code owner config file issues are returned.
-   *
-   * <p>The following values are supported:
-   *
-   * <ul>
    *   <li>{@code FATAL}: only fatal issues are returned
    *   <li>{@code ERROR}: only fatal and error issues are returned
    *   <li>{@code WARNING}: all issues (warning, error and fatal) are returned
