8994b82cc62e145230681517231bb0ab1c58918e
Dhruv Srivastava <dhruvsri@google.com>
2023-06-12 09:01:06 +0000

    sss
    Change-Id: I37f80824576722cc8fbedabcd68203de2cf03e0b

--- java/com/google/gerrit/plugins/codeowners/api/CheckCodeOwnerConfigFilesInRevisionInput.java
+++ java/com/google/gerrit/plugins/codeowners/api/CheckCodeOwnerConfigFilesInRevisionInput.java
@@ -3,8 +3,6 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 //
    *   <li>{@code FATAL}: only fatal issues are returned
-   *   <li>{@code ERROR}: only fatal and error issues are returned
-   *   <li>{@code WARNING}: all issues (warning, error and fatal) are returned
    * </ul>
    *
    * <p>If unset, {@code WARNING} is used.
