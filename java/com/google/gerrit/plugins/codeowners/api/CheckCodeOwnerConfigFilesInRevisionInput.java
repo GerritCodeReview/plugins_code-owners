d44391f7898e57b0e30dd78a1663a7aca881a9fb
Dhruv Srivastava <dhruvsri@google.com>
2023-06-12 08:58:41 +0000

    asdasdasd
    Change-Id: I37f80824576722cc8fbedabcd68203de2cf03e0b

--- java/com/google/gerrit/plugins/codeowners/api/CheckCodeOwnerConfigFilesInRevisionInput.java
+++ java/com/google/gerrit/plugins/codeowners/api/CheckCodeOwnerConfigFilesInRevisionInput.java
@@ -2,9 +2,6 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 //
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
    *   <li>{@code FATAL}: only fatal issues are returned
    *   <li>{@code ERROR}: only fatal and error issues are returned
    *   <li>{@code WARNING}: all issues (warning, error and fatal) are returned
