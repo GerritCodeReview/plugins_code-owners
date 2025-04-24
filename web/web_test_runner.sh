fb04a189c603bf473f3c8413a14b4c10641944c2
Paladox none <thomasmulhall410@yahoo.com>
2025-04-24 08:19:47 -0700

    Fix typo
    Change-Id: Id0d23eba1f67d13ea8c16b17ee4fd9f8bcb94937

--- web/web_test_runner.sh
+++ web/web_test_runner.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 ./$1 --config $2 \
   --dir 'plugins/code-owners/web/_bazel_ts_out_tests' \
   --test-files 'plugins/code-owners/web/_bazel_ts_out_tests/*_test.js' \
-  --type-config="plugins/code-owners/web/tsconfig.json"
+  --ts-config="plugins/code-owners/web/tsconfig.json"
